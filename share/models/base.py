47a963db1df2b1ee6c1c892652670fb3ec9c009a
Chris Seto <chriskseto@gmail.com>
2016-06-03 14:49:20 -0400

    Move versioning into model logic

--- share/models/base.py
+++ share/models/base.py
@@ -1,57 +1,48 @@
+import uuid
 import inspect
 
 from django.db import models
+from django.db import transaction
 from django.db.models.base import ModelBase
 from django.contrib.auth.models import User
-
-
-class ShareVersion:
-    id = models.AutoField(primary_key=True)
-    created_by = models.ForeignKey(User)
-    created_at = models.DateTimeField()#(auto_add_now=True)
-    persistant_id = models.CharField(max_length=256)
-
-    class Meta:
-        abstract = True
-
-
-class ShareCurrent:
-    id = models.CharField(max_length=256, primary_key=True)  # Persistant ID
 
 
 class ShareAbstract:
     pass
 
 
-class ShareRelation:
+class ShareVersion(models.Model):
+    id = models.AutoField(primary_key=True)
+    # created_by = models.ForeignKey(User)  TODO
+    created_at = models.DateTimeField(auto_now=True)
+    persistant_id = models.UUIDField()  # TODO Maybe make this a foreign Key
 
-    @property
-    def django(self):
-        return self.django_class(self.model, **self.kwargs)
-
-    @property
-    def version(self):
-        if not issubclass(self.model, ShareModel):
-            return self.django
-        return self.django_class(self.model.Version, **self.kwargs)
-
-    @property
-    def current(self):
-        if not issubclass(self.model, ShareModel):
-            return self.django
-        return self.django_class(self.model.Current, **self.kwargs)
-
-    def __init__(self, model, **kwargs):
-        self.model = model
-        self.kwargs = kwargs
+    class Meta:
+        abstract = True
 
 
-class ShareManyToManyField(ShareRelation):
-    django_class = models.ManyToManyField
+class ShareCurrent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
 
 
-class ShareForeignKey(ShareRelation):
-    django_class = models.ForeignKey
+class ShareConcrete(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+    # TODO Tag w/ user
+    @transaction.atomic
+    def save(self):
+        data = {field.attname: getattr(self, field.attname) for field in self._meta.fields}
+        data['persistant_id'] = data.pop('id')
+        version = self.__class__.Version(**data)
+        version.save()
+        self.__class__.Current(id=self.id, version=version).save()
+        return super().save()
 
 
 class ShareMeta(ModelBase):
@@ -66,37 +57,22 @@ class ShareMeta(ModelBase):
             attrs['Meta'] = type('Meta', (object, ), {})
         attrs['Meta'].abstract = True
 
-        for k, v in tuple(attrs.items()):
-            if not isinstance(v, ShareRelation):
-                continue
-            attrs[k] = v.current
-
-        class ConcreteMeta(attrs['Meta']):
-            managed = False
-
-        class Meta(attrs['Meta']):
-            managed = True
-
         attrs['__qualname__'] = 'Abstract' + attrs['__qualname__']
-        abstract = super(ShareMeta, cls).__new__(cls, 'Abstract' + name, bases, attrs)
-        concrete = type(name, (abstract, ShareAbstract), {'__module__': module})
+        abstract = super(ShareMeta, cls).__new__(cls, 'Abstract' + name, bases + (ShareAbstract, ), attrs)
+        concrete = type(name, (ShareConcrete, abstract), {'__module__': module})
 
         concrete.Abstract = abstract
 
         concrete.Version = type(
             name + 'Version',
-            (abstract, ShareVersion),
-            {
-                'Meta': Meta,
-                '__module__': module,
-            }
+            (ShareVersion, abstract),
+            {'__module__': module}
         )
 
         concrete.Current = type(
             name + 'Current',
-            (abstract, ShareCurrent),
+            (ShareCurrent, abstract),
             {
-                'Meta': Meta,
                 '__module__': module,
                 'version': models.ForeignKey(concrete.Version),
             }
@@ -109,6 +85,7 @@ class ShareMeta(ModelBase):
         })
 
         return concrete
+
 
 class ShareModel(models.Model, metaclass=ShareMeta):
 
