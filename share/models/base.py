8a83357633a2b04784962beeab6e76b4aa8a3983
Chris Seto <chriskseto@gmail.com>
2016-06-07 16:56:48 -0400

    Replace/update ShareModel with ShareObject

--- share/models/base.py
+++ share/models/base.py
@@ -5,144 +5,98 @@ from django.db import models
 from django.conf import settings
 from django.db import transaction
 from django.db.models.base import ModelBase
+from django.db.models.fields.related import lazy_related_operation
 
 from share.models.core import RawData
+from share.models.core import ShareUser
 
 
-class ShareRelation:
+class AbstractShareObject(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    DJANGO_FIELD = None
-
-    def __init__(self, related):
-        assert issubclass(related, ShareModel), 'ShareRelations may only be used with ShareModels. Got {!r}'.format(related)
-        self.related = related
-
-    def for_version(self, **kwargs):
-        return self.DJANGO_FIELD(self.related.Current, **kwargs)
-
-    def for_concrete(self, **kwargs):
-        return self.DJANGO_FIELD(self.related, **kwargs)
-
-    def for_current(self, **kwargs):
-        return None
-
-
-class ShareForeignKey(ShareRelation):
-    DJANGO_FIELD = models.ForeignKey
-
-
-class ShareManyToMany(ShareRelation):
-
-    DJANGO_FIELD = models.ManyToManyField
-
-    def __init__(self, related, through):
-        self.through = through
-        super().__init__(related)
-
-    def for_version(self, **kwargs):
-        return None
-        # import ipdb; ipdb.set_trace()
-        # return super().for_version(**{**kwargs, 'through': self.through + 'Version'})
-
-    def for_concrete(self, **kwargs):
-        return super().for_concrete(**{**kwargs, 'through': self.through})
-
-    def for_current(self, **kwargs):
-        return self.DJANGO_FIELD(self.related.Current, **{**kwargs, 'through': self.through + 'Version'})
-
-
-class ShareAbstract:
-    pass
-
-
-class ShareVersion(models.Model):
-    id = models.AutoField(primary_key=True)
+    source = models.ForeignKey(ShareUser)
     source_data = models.ForeignKey(RawData, blank=True, null=True)  # NULL/None indicates a user submitted change
-    source = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="%(app_label)s_%(class)s_related")
-    created_at = models.DateTimeField(auto_now=True)
-    persistant_id = models.UUIDField()  # TODO Maybe make this a foreign Key
 
-    class Meta:
-        abstract = True
-
-
-class ShareCurrent(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
-    class Meta:
-        abstract = True
-
-
-class ShareConcrete(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    source_data = models.ForeignKey(RawData, blank=True, null=True)  # NULL/None indicates a user submitted change
-    source = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="%(app_label)s_%(class)s_related")
+    changed_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
 
-class ShareMeta(ModelBase):
+class ShareObjectVersion(models.Model):
+    action = models.CharField(max_length=10);
+    persistant_id = models.PositiveIntegerField()  # Must match the id of ShareObject
+
+    class Meta:
+        abstract = True
+
+
+class ShareForeignKey(models.ForeignKey):
+
+    def __init__(self, model, **kwargs):
+        self.__kwargs = kwargs
+        super().__init__(model, **kwargs)
+
+    def contribute_to_class(self, cls, name, **kwargs):
+        actual = self.__class__.mro()[1](self.remote_field.model, **self.__kwargs)
+        actual.contribute_to_class(cls, name, **kwargs)
+
+        self.__kwargs['editable'] = False
+        version = self.__class__.mro()[1](self.remote_field.model.VersionModel, **self.__kwargs)
+        version.contribute_to_class(cls, name + '_version', **kwargs)
+
+class ShareManyToMany(models.ManyToManyField):
+
+    def __init__(self, model, **kwargs):
+        self.__kwargs = kwargs
+        super().__init__(model, **kwargs)
+
+    def contribute_to_class(self, cls, name, **kwargs):
+        actual = self.__class__.mro()[1](self.remote_field.model, **self.__kwargs)
+        actual.contribute_to_class(cls, name, **kwargs)
+
+        self.__kwargs['through'] += 'Version'
+        self.__kwargs['editable'] = False
+
+        version = self.__class__.mro()[1](self.remote_field.model.VersionModel, **self.__kwargs)
+        version.contribute_to_class(cls, name[:-1] + '_versions', **kwargs)
+
+
+class ShareObjectMeta(ModelBase):
 
     def __new__(cls, name, bases, attrs):
         if models.Model in bases or len(bases) > 1:
-            return super(ShareMeta, cls).__new__(cls, name, bases, attrs)
-
+            return super(ShareObjectMeta, cls).__new__(cls, name, bases, attrs)
         module = attrs['__module__']
-
-        relations = {}
-        for k, v in tuple(attrs.items()):
-            if isinstance(v, ShareRelation):
-                relations[k] = attrs.pop(k)
 
         if not attrs.get('Meta'):
             attrs['Meta'] = type('Meta', (object, ), {})
         attrs['Meta'].abstract = True
 
         attrs['__qualname__'] = 'Abstract' + attrs['__qualname__']
-        abstract = super(ShareMeta, cls).__new__(cls, 'Abstract' + name, bases + (ShareAbstract, ), attrs)
+        abstract = super(ShareObjectMeta, cls).__new__(cls, 'Abstract' + name, (AbstractShareObject, ), attrs)
 
-        concrete = type(
-            name,
-            (ShareConcrete, abstract),
-            {
-                '__module__': module,
-                **{k: v.for_concrete() for k, v in relations.items() if v}
-            }
-        )
-
-        concrete.Abstract = abstract
-
-        concrete.Version = type(
+        version = type(
             name + 'Version',
-            (ShareVersion, abstract),
-            {
-                '__module__': module,
-                **{k: v.for_version() for k, v in relations.items() if v}
-            }
+            (abstract, ShareObjectVersion),
+            {'__module__': module}
         )
 
-        concrete.Current = type(
-            name + 'Current',
-            (ShareCurrent, ),
-            {
-                '__module__': module,
-                'version': models.ForeignKey(concrete.Version),
-                **{k: v.for_current() for k, v in relations.items() if v}
-            }
-        )
-
-        inspect.stack()[1].frame.f_globals.update({
-            concrete.Abstract.__name__: concrete.Abstract,
-            concrete.Version.__name__: concrete.Version,
-            concrete.Current.__name__: concrete.Current,
+        concrete = super(ShareObjectMeta, cls).__new__(cls, name, (abstract, ShareObject), {
+            '__module__': module,
+            'version': models.OneToOneField(version, on_delete=models.PROTECT, related_name='%(app_label)s_%(class)s_version')
         })
+
+        concrete.VersionModel = version
+
+        inspect.stack()[1].frame.f_globals.update({concrete.VersionModel.__name__: concrete.VersionModel})
 
         return concrete
 
 
-class ShareModel(models.Model, metaclass=ShareMeta):
+class ShareObject(models.Model, metaclass=ShareObjectMeta):
+    id = models.AutoField(primary_key=True)
 
     class Meta:
         abstract = True
