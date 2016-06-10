e869b42acd06b40c4e441ac89a9149d9af659376
Chris Seto <chriskseto@gmail.com>
2016-06-10 15:56:41 -0400

    Remove unused code

--- raw/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RawConfig(AppConfig):
-    name = 'raw'
--- raw/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.postgres.fields import JSONField
-
-
-class Raw(models.Model):
-    harvester = models.CharField(max_length=200)
-    harvester_version = models.CharField(max_length=200)
-    date_harvested = models.DateTimeField('date harvested')
-    data = JSONField()
