590609064b80a574eae696d6685605ff50d9f9a5
Chris Seto <chriskseto@gmail.com>
2016-06-10 12:59:57 -0400

    Add a command for running harvesters

---
+++ share/management/commands/harvest.py
@@ -0,0 +1,17 @@
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+from share.tasks import run_harvester
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('harvester', type=str, help='The name of the harvester to run')
+        parser.add_argument('--async', action='store_true', help='Whether or not to use Celery')
+
+    def handle(self, *args, **options):
+        if options['async']:
+            run_harvester.apply_async((options['harvester'],))
+        else:
+            run_harvester(options['harvester'])
