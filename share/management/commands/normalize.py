5006026779ead7cb2f0fbc48f63d1cd2371429a6
Chris Seto <chriskseto@gmail.com>
2016-06-16 09:55:47 -0400

    Harvester updates

--- share/management/commands/normalize.py
+++ share/management/commands/normalize.py
@@ -1,16 +1,16 @@
 from django.core.management.base import BaseCommand
 
-from share.tasks import run_harvester
+from share.tasks import run_normalizer
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('harvester', type=str, help='The name of the harvester to run')
+        parser.add_argument('normalizer', type=str, help='The name of the normalizer to run')
         parser.add_argument('--async', action='store_true', help='Whether or not to use Celery')
 
     def handle(self, *args, **options):
         if options['async']:
-            run_harvester.apply_async((options['harvester'],))
+            run_normalizer.apply_async((options['normalizer'],))
         else:
-            run_harvester(options['harvester'])
+            run_normalizer(options['normalizer'])
--- share/tasks.py
+++ share/tasks.py
@@ -1,11 +1,32 @@
 import datetime
+
 import celery
 
 from django.apps import apps
 
+from share.models import RawData
+
 
 @celery.task
-def run_harvester(app_label):
+def run_harvester(app_label, started_by=None):
     config = apps.get_app_config(app_label)
     harvester = config.HARVESTER(config)
     harvester.harvest(datetime.timedelta(days=-1), datetime.datetime.utcnow())
+
+
+@celery.task
+def run_normalizer(app_label, started_by=None):
+    config = apps.get_app_config(app_label)
+    normalizer = config.as_normalizer()
+
+    for block in normalizer.blocks(size=50):
+        normalize_block(app_label, block, started_by=started_by)
+
+
+@celery.task
+def normalize_block(app_label, block, started_by=None):
+    config = apps.get_app_config(app_label)
+    normalizer = config.as_normalizer()
+
+    for raw in RawData.objects.filter(id__in=block):
+        normalizer.normalize(raw)
