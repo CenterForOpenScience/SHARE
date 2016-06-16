189ceb421e85bf5089f301574a515e5d9742e54b
icereval <icereval@gmail.com>
2016-06-16 19:13:02 -0400

    share admin models

--- share/admin.py
+++ share/admin.py
@@ -1,10 +1,10 @@
-# from django.contrib import admin
-#
-# from .models import Contributor, Organization, Affiliation, Email, Manuscript
+from django.contrib import admin
+
+from .models import Organization, Affiliation, Email, Manuscript
 # from providers.org.arxiv_api.models import ArxivManuscripts
-# admin.site.register(Contributor)
-# admin.site.register(Organization)
-# admin.site.register(Affiliation)
-# admin.site.register(Email)
-# admin.site.register(Manuscript)
+
+admin.site.register(Organization)
+admin.site.register(Affiliation)
+admin.site.register(Email)
+admin.site.register(Manuscript)
 # admin.site.register(ArxivManuscripts)
