186b727a216fb642f82b3c9fbf514f54eee512aa
Chris Seto <chriskseto@gmail.com>
2016-06-21 00:42:52 -0400

    Remove unused providers

--- providers/edu/asu/apps.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class ClinicalTrialsConfig(ProviderAppConfig):
-    name = 'providers.edu.asu'
-    TITLE = 'asu'
-    HARVESTER = FigshareHarvester
-
-    SCHEDULE = crontab(minute=0, hour=0)
--- providers/org/example/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'providers.org.example.apps.ExampleConfig'
