922023654a5ffd18220b8e3b1658aebc9a4d6359
Chris Seto <chriskseto@gmail.com>
2016-06-22 13:32:42 -0400

    Update up.sh

--- up.sh
+++ up.sh
@@ -2,14 +2,17 @@
 
 # rm -f share/migrations/00*.py
 # rm -f providers/*/*/migrations/0*.py
-git clean -f
+# && touch */**/migrations/__init__.py \
+# rm -f share/migrations/* \
+# && touch share/migrations/__init__.py \
+# && git clean -Xf providers/*/*/migrations/0*.py \
+rm -fv ./{*/*/,}*/migrations/00*.py
 
-python manage.py reset_db --noinput
-
-python manage.py makemigrations
-python manage.py maketriggermigrations
-python manage.py makeprovidermigrations
-
-python manage.py migrate
-
-python manage.py createsuperuser
+python manage.py reset_db --noinput \
+&& python manage.py makemigrations \
+&& git checkout api/migrations \
+&& git checkout share/migrations \
+&& python manage.py maketriggermigrations \
+&& python manage.py makeprovidermigrations \
+&& python manage.py migrate \
+&& python manage.py createsuperuser
