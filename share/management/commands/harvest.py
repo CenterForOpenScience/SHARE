462ce72453d239059c169938314fb4c910c33756
Chris Seto <chriskseto@gmail.com>
2016-06-24 11:56:14 -0400

    Add --all to harvest

--- share/management/commands/harvest.py
+++ share/management/commands/harvest.py
@@ -1,14 +1,17 @@
 import datetime
 
+from django.apps import apps
 from django.core.management.base import BaseCommand
 
 from share.tasks import run_harvester
+from share.provider import ProviderAppConfig
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('harvester', type=str, help='The name of the harvester to run')
+        parser.add_argument('--all', action='store_true', help='Run all harvester')
+        parser.add_argument('harvester', nargs='*', type=str, help='The name of the harvester to run')
         parser.add_argument('--async', action='store_true', help='Whether or not to use Celery')
         parser.add_argument('--days-back', type=int, help='The number of days to go back')
 
@@ -18,7 +21,13 @@ class Command(BaseCommand):
             kwargs['end'] = datetime.datetime.utcnow() + datetime.timedelta(days=-(options['days_back'] - 1))
             kwargs['start'] = datetime.datetime.utcnow() + datetime.timedelta(days=-options['days_back'])
 
-        if options['async']:
-            run_harvester.apply_async((options['harvester'],), **kwargs)
-        else:
-            run_harvester(options['harvester'], **kwargs)
+        if not options['harvester'] and options['all']:
+            options['harvester'] = [x.label for x in apps.get_app_configs() if isinstance(x, ProviderAppConfig)]
+
+        for harvester in options['harvester']:
+            if options['async']:
+                run_harvester.apply_async((harvester,), **kwargs)
+                self.stdout.write('Started job for harvester {}'.format(harvester))
+            else:
+                self.stdout.write('Running harvester for {}'.format(harvester))
+                run_harvester(harvester, **kwargs)
