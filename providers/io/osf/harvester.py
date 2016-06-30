996891156f9872fa79d06f8f3896bc6ab376c519
erinspace <erin.braswell@gmail.com>
2016-06-30 11:52:46 -0400

    Basic OSF harvester

--- providers/io/osf/harvester.py
+++ providers/io/osf/harvester.py
@@ -1,5 +1,40 @@
+import arrow
+from furl import furl
+from typing import Tuple
+from typing import Union
+from typing import Iterator
+
 from share.harvest.harvester import Harvester
 
 
 class OSFHarvester(Harvester):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.url = 'https://api.osf.io/v2/nodes/?filter[public]=true'
+
+    def do_harvest(self, start_date: arrow.Arrow, end_date: arrow.Arrow) -> Iterator[Tuple[str, Union[str, dict, bytes]]]:
+        url = furl(self.url)
+
+        url.args['filter[date_updated][gt]'] = start_date.date().isoformat()
+        url.args['filter[date_updated][lt]'] = end_date.date().isoformat()
+
+        return self.fetch_records(url)
+
+
+def fetch_records(self, url: furl) -> list:
+        records = self.requests.get(url.url)
+
+        total_records = records.json()['meta']['total']
+        all_records = []
+        while records['links'].get('next'):
+            record_list = records.json()['data']
+
+            for record in record_list:
+                all_records.append(record)
+
+            records = self.requests.get(records.json()['links']['next'])
+
+        total = int(records.json()['counts']['total'])
+
+        return all_records
