a5addf7d74ab22512ef2dc6fb1056de8ff74824d
Casey Rollins <cmr3rm@virginia.edu>
2016-06-30 16:53:09 -0400

    CrossRef normalizer.

--- providers/org/crossref/normalizer.py
+++ providers/org/crossref/normalizer.py
@@ -1,1 +1,25 @@
-__author__ = 'casey'
+from share.normalize import *
+
+
+class CreativeWork(Parser):
+    # Dates in CrossRef metadata are often incomplete, see: https://github.com/CrossRef/rest-api-doc/blob/master/rest_api.md#notes-on-dates
+    title = ctx.title[0]
+    description = ctx.maybe('subtitle')
+    doi = ctx.DOI
+    contributors = ctx.author['*']
+    tags = ctx.maybe('subject')['*']
+
+
+class Contributor(Parser):
+    person = ctx
+    order_cited = ctx['index']
+
+
+class Person(Parser):
+    family_name = ctx.family
+    given_name = ctx.given
+    affiliations = ctx.affiliation['*']
+
+
+class Organization(Parser):
+    name = ctx.name
