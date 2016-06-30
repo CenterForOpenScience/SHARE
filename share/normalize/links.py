88f44d860e9cf334ba8294d72e7ff861595158db
Chris Seto <chriskseto@gmail.com>
2016-06-30 13:31:42 -0400

    Magic away the need to do [0] all the time and sometimes .text()

--- share/normalize/links.py
+++ share/normalize/links.py
@@ -235,7 +235,13 @@ class PathLink(AbstractLink):
             # Dirty hack to avoid namespaces with xpath
             # Anything name "<namespace>:<node>" will be accessed as <node>
             # IE: oai:title -> title
-            return obj.xpath('./*[local-name()=\'{}\']'.format(self._segment))
+            ret = obj.xpath('./*[local-name()=\'{}\']'.format(self._segment))
+            if len(ret) == 1 and not isinstance(self._next, (IndexLink, IteratorLink)):
+                ret = ret[0]
+                if self._next is None:
+                    ret = ret.text()
+            return ret
+
         return obj[self._segment]
 
     def __repr__(self):
