24fd503e85849a5f4220abbeb3d740359cc651ca
Chris Wisecarver <cwisecarver@cos.io>
2016-06-30 20:23:06 -0400

    Add Link model.

---
+++ share/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+from model_utils import Choices
+from django.utils.translation import ugettext as _
+
+class ShareConfig(AppConfig):
+    name = 'share'
+    link_type_choices = Choices(
+        (0, 'doi', _('DOI')),
+        (1, 'orcid', _('Orchid')),
+        (2, 'misc', _('Miscellaneous'))
+    )
