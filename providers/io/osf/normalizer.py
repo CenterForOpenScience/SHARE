10aab3b949a81bb8d11451c44c8426e3e2fdc661
erinspace <erin.braswell@gmail.com>
2016-07-01 11:04:04 -0400

    Add more to the OSF normalizer

--- providers/io/osf/normalizer.py
+++ providers/io/osf/normalizer.py
@@ -1,5 +1,3 @@
-from share.normalize import *
-
 from share.normalize import ctx
 from share.normalize.parsers import Parser
 
@@ -8,21 +6,32 @@ class CreativeWork(Parser):
     title = ctx.attributes.title
     description = ctx.attributes.description
     contributors = ctx.contributors['*']
-    institutions = ctx.contributors.institutions['*']
+    institutions = ctx.relationships.affiliated_institutions['*']
     created = ctx.attributes.date_created
     subject = ctx.attributes.category
+    tags = ctx.attributes.tags['*']
 
 
 class Contributor(Parser):
     person = ctx
+    order_cited = ctx['index']
+    cited_name = ctx.embeds.users.data.attributes.full_name
+
+
+class Tag(Parser):
+    name = ctx
 
 
 class Person(Parser):
-    given_name = ctx.contributor.embeds.users.data.attributes.given_name
-    family_name = ctx.contributor.embeds.users.data.attributes.family_name
-    additional_name = ctx.contributor.embeds.users.data.attributes.middle_names
-    suffix = ctx.contributor.embeds.users.data.attributes.suffix
+    given_name = ctx.embeds.users.data.attributes.given_name
+    family_name = ctx.embeds.users.data.attributes.family_name
+    additional_name = ctx.embeds.users.data.attributes.middle_names
+    suffix = ctx.embeds.users.data.attributes.suffix
 
 
-class Instutions(Parser):
-    url = ctx.links.self.href
+class ThroughInstitutions(Parser):
+    institution = ctx
+
+class Institution(Parser):
+    name = ''
+    url = ctx.links.related.href
