1d6b0cdd6690f11513581932650aff04a515c8d4
erinspace <erin.braswell@gmail.com>
2016-07-01 14:27:45 -0400

    add args via furl instead of hard-coding

--- providers/io/osf/harvester.py
+++ providers/io/osf/harvester.py
@@ -15,12 +15,15 @@ class OSFHarvester(Harvester):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = 'https://api.osf.io/v2/nodes/?page[size]=100&filter[public]=true&embed=affiliated_institutions'
+        self.url = 'https://api.osf.io/v2/nodes/'
 
     def do_harvest(self, start_date: arrow.Arrow, end_date: arrow.Arrow) -> Iterator[Tuple[str, Union[str, dict, bytes]]]:
 
         url = furl(self.url)
 
+        url.args['page[size]'] = 100
+        url.args['filter[public]'] = 'true'
+        url.args['embed'] = 'affiliated_institutions'
         url.args['filter[date_modified][gt]'] = start_date.date().isoformat()
         url.args['filter[date_modified][lt]'] = end_date.date().isoformat()
 
