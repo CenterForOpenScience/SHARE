e7e2cb858376626798a6440a59d548a523e579a3
erinspace <erin.braswell@gmail.com>
2016-07-04 10:22:56 -0400

    Rearrange parsers to make sense in executing order

--- providers/io/osf/normalizer.py
+++ providers/io/osf/normalizer.py
@@ -1,53 +1,6 @@
 from share.normalize import ctx
 from share.normalize.parsers import Parser
-
-
-class CreativeWork(Parser):
-    title = ctx.attributes.title
-    description = ctx.attributes.description
-    contributors = ctx.contributors['*']
-    institutions = ctx.embeds.affiliated_institutions.data['*']
-    created = ctx.attributes.date_created
-    subject = ctx.attributes.category
-    tags = ctx.attributes.tags['*']
-
-    class Extra:
-        files = ctx.relationships.files.links.related.href
-        parent = ctx.relationships.maybe('parent.links.related.href')
-        forks = ctx.relationships.forks.links.related.href
-        root = ctx.relationships.root.links.related.href
-        comments = ctx.relationships.comments.links.related.href
-        registrations = ctx.relationships.registrations.links.related.href
-        logs = ctx.relationships.logs.links.related.href
-        node_links = ctx.relationships.node_links.links.related.href
-        wikis = ctx.relationships.wikis.links.related.href
-        children = ctx.relationships.children.links.related.href
-        fork = ctx.attributes.fork
-        date_modified = ctx.attributes.date_modified
-        collection = ctx.attributes.collection
-        registration = ctx.attributes.registration
-        node_license = ctx.attributes.node_license
-        type = ctx.type
-        id = ctx.id
-
-
-class Contributor(Parser):
-    person = ctx
-    order_cited = ctx['index']
-    cited_name = ctx.embeds.users.data.attributes.full_name
-
-
-class ThroughTags(Parser):
-    tag = ctx
-
-
-class Tag(Parser):
-    name = ctx
-    type = ctx
-
-
-class Taxonomy(Parser):
-    name = ctx
+from share.normalize.links import Delegate, Map, Maybe
 
 
 class Person(Parser):
@@ -66,8 +19,23 @@ class Person(Parser):
         profile_image = ctx.embeds.users.data.links.profile_image
 
 
-class ThroughInstitutions(Parser):
-    institution = ctx
+class Contributor(Parser):
+    person = Delegate(Person, ctx)
+    order_cited = ctx['index']
+    cited_name = ctx.embeds.users.data.attributes.full_name
+
+
+class Taxonomy(Parser):
+    name = ctx
+
+
+class Tag(Parser):
+    name = ctx
+    type = Delegate(Taxonomy, ctx)
+
+
+class ThroughTags(Parser):
+    tag = Delegate(Tag, ctx)
 
 
 class Institution(Parser):
@@ -79,3 +47,44 @@ class Institution(Parser):
         users = ctx.relationships.users.links.related.href
         registrations = ctx.relationships.registrations.links.related.href
         description = ctx.attributes.description
+
+
+class ThroughInstitutions(Parser):
+    institution = Delegate(ctx, Institution)
+
+
+class CreativeWork(Parser):
+    title = ctx.attributes.title
+    description = ctx.attributes.description
+
+    # contributors = ctx.contributors['*']
+    contributors = Map(Delegate(Contributor), ctx['contributors'])
+
+    # institutions = ctx.embeds.affiliated_institutions.data['*']
+    institutions = Map(Delegate(ThroughInstitutions), ctx.embeds.affiliated_institutions.data)
+
+    created = ctx.attributes.date_created
+
+    # subject = ctx.attributes.category
+    # tags = ctx.attributes.tags['*']
+    subject = Delegate(Tag, ctx.attributes.category)
+    tags = Map(Delegate(ThroughTags), ctx.attributes.tags)
+
+    class Extra:
+        files = ctx.relationships.files.links.related.href
+        parent = Maybe(ctx, 'relationships.parent.links.related.href')
+        forks = ctx.relationships.forks.links.related.href
+        root = ctx.relationships.root.links.related.href
+        comments = ctx.relationships.comments.links.related.href
+        registrations = ctx.relationships.registrations.links.related.href
+        logs = ctx.relationships.logs.links.related.href
+        node_links = ctx.relationships.node_links.links.related.href
+        wikis = ctx.relationships.wikis.links.related.href
+        children = ctx.relationships.children.links.related.href
+        fork = ctx.attributes.fork
+        date_modified = ctx.attributes.date_modified
+        collection = ctx.attributes.collection
+        registration = ctx.attributes.registration
+        node_license = ctx.attributes.node_license
+        type = ctx.type
+        id = ctx.id
