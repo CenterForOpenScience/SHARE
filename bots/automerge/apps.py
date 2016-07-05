7c333eb7f0ca0221859581e8bbef7c9924854221
Chris Seto <chriskseto@gmail.com>
2016-07-05 15:39:35 -0400

    Add versions to bots

--- bots/automerge/apps.py
+++ bots/automerge/apps.py
@@ -3,6 +3,7 @@ from share.bot import BotAppConfig
 
 class AppConfig(BotAppConfig):
     name = 'bots.automerge'
+    version = '0.0.1'
 
     def get_bot(self):
         from bots.automerge.bot import AutoMergeBot
