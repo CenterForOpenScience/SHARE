ea818cd541425b468007da188b74558ce6a06316
icereval <icereval@gmail.com>
2016-07-05 19:01:27 -0400

    backup/restore rawdata on up.sh run

--- up.sh
+++ up.sh
@@ -4,6 +4,7 @@ set -e
 rm -fv ./{*/*/*/,*/*/,}*/migrations/00*.py
 rm -fv ./bots/*/migrations/00*.py
 
+python manage.py dumpdata share.RawData share.CeleryTask --natural-foreign --natural-primary --format json --output share_rawdata.json || true
 python manage.py celery purge -f
 python manage.py reset_db --noinput
 python manage.py makemigrations
@@ -14,3 +15,4 @@ python manage.py maketriggermigrations
 python manage.py makeprovidermigrations
 python manage.py migrate
 python manage.py loaddata ./share/models/initial_data.yaml
+python manage.py loaddata share_rawdata.json
