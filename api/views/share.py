26b74b0f4975e7136bf133fe46370246d8c67fcf
Chris Wisecarver <cwisecarver@cos.io>
2016-07-04 22:09:19 -0400

    Add custom queries to improve performance ... a lil

--- api/views/share.py
+++ api/views/share.py
@@ -17,85 +17,89 @@ class ExtraDataViewSet(ShareObjectViewSet):
 
 class EntityViewSet(ShareObjectViewSet):
     serializer_class = serializers.EntitySerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class VenueViewSet(ShareObjectViewSet):
     serializer_class = serializers.VenueSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class OrganizationViewSet(ShareObjectViewSet):
     serializer_class = serializers.OrganizationSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class PublisherViewSet(ShareObjectViewSet):
     serializer_class = serializers.PublisherSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class InstitutionViewSet(ShareObjectViewSet):
     serializer_class = serializers.InstitutionSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class IdentifierViewSet(ShareObjectViewSet):
     serializer_class = serializers.IdentifierSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class PersonViewSet(ShareObjectViewSet):
     serializer_class = serializers.PersonSerializer
-    queryset = serializer_class.Meta.model.objects.prefetch_related('emails', 'affiliations', 'identifiers')
+    queryset = serializer_class.Meta.model.objects.select_related(
+        'extra'
+    ).prefetch_related(
+        'emails',
+        'affiliations',
+        'identifiers'
+    )
 
 
 class AffiliationViewSet(ShareObjectViewSet):
     serializer_class = serializers.AffiliationSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra', 'person', 'entity')
 
 
 class ContributorViewSet(ShareObjectViewSet):
     serializer_class = serializers.ContributorSerializer
-    queryset = serializer_class.Meta.model.objects.select_related('person')
+    queryset = serializer_class.Meta.model.objects.select_related('extra', 'person', 'creative_work')
 
 
 class FunderViewSet(ShareObjectViewSet):
     serializer_class = serializers.FunderSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class AwardViewSet(ShareObjectViewSet):
     serializer_class = serializers.AwardSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class TagViewSet(ShareObjectViewSet):
     serializer_class = serializers.TagSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related('extra')
 
 
 class CreativeWorkViewSet(ShareObjectViewSet):
     serializer_class = serializers.CreativeWorkSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related(
+        'subject',
+        'extra'
+    )
 
 
 class PreprintViewSet(ShareObjectViewSet):
     serializer_class = serializers.PreprintSerializer
     queryset = serializer_class.Meta.model.objects.select_related(
-        'subject'
-    ).prefetch_related(
-            'contributors',
-            'awards',
-            'venues',
-            'links',
-            'funders',
-            'publishers',
-            'institutions',
-            'tags'
+        'subject',
+        'extra'
     )
 
 
 class ManuscriptViewSet(ShareObjectViewSet):
     serializer_class = serializers.ManuscriptSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+    queryset = serializer_class.Meta.model.objects.all().select_related(
+        'subject',
+        'extra'
+    )
