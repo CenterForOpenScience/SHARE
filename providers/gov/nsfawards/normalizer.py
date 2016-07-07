3c4bac3ac40c865b515e53196f057632c4a6b89e
erinspace <erin.braswell@gmail.com>
2016-07-07 11:38:27 -0400

    Fix the nsfawards normalizer to fit with new system

--- providers/gov/nsfawards/normalizer.py
+++ providers/gov/nsfawards/normalizer.py
@@ -1,6 +1,6 @@
 from share.normalize import ctx
+from share.normalize import tools
 from share.normalize.parsers import Parser
-from share.normalize.links import Delegate, Map, Maybe, Concat
 
 
 class Person(Parser):
@@ -9,50 +9,75 @@ class Person(Parser):
 
 
 class Contributor(Parser):
-    person = Delegate(Person, ctx)
+    person = tools.Delegate(Person, ctx)
     order_cited = ctx('index')
-    cited_name = ctx
+    cited_name = tools.Join(tools.Concat(ctx.piFirstName, ctx.piLastName), joiner=' ')
 
 
 class Institution(Parser):
-    name = ctx
-    # TODO - fix the URL with a custum callable and an ID
-    url = ctx
+    name = ctx.agency
 
 
-class ThroughInstitutions(Parser):
-    institution = Delegate(ctx, Institution)
+class Association(Parser):
+    pass
 
 
 class Award(Parser):
     description = ctx.fundsObligatedAmt
-    # TODO - add a url and custom callable
+    url = tools.RunPython('format_url', ctx)
+
+    def format_url(self, ctx):
+        return 'https://www.nsf.gov/awardsearch/showAward?AWD_ID={}'.format(ctx['id'])
 
     class Extra:
         awardee_city = ctx.awardeeCity
+        funds_obligated_amt = ctx.fundsObligatedAmt
+        name = ctx.awardeeName
+        awardee_city = ctx.awardeeCity
+        awardee_state_code = ctx.awardeeStateCode
 
 
 class ThroughAwards(Parser):
-    award = Delegate(Award, ctx)
+    award = tools.Delegate(Award, ctx)
 
 
 class Venue(Parser):
-    name = Concat(ctx.awardeeCity, ctx.awardeeStateCode)
+    name = ctx.awardeeName
+    location = tools.Join(tools.Concat(ctx.awardeeCity, ctx.awardeeStateCode), joiner=', ')
+
+    class Extra:
+        awardee_city = ctx.awardeeCity
+        awardee_state_code = ctx.awardeeStateCode
 
 
-class ThroughVenues(Parser)
-    venue = Delegate(Venue, ctx)
+class ThroughVenues(Parser):
+    venue = tools.Delegate(Venue, ctx)
+
+
+class Link(Parser):
+    url = tools.RunPython('format_url', ctx)
+    type = 'provider'
+
+    def format_url(self, ctx):
+        return 'https://www.nsf.gov/awardsearch/showAward?AWD_ID={}'.format(ctx['id'])
+
+
+class ThroughLinks(Parser):
+    link = tools.Delegate(Link, ctx)
 
 
 class CreativeWork(Parser):
     title = ctx.title
-    contributors = Map(Delegate(Contributor), Concat(ctx.piFirstName, ctx.piLastName))
+    contributors = tools.Map(tools.Delegate(Contributor), ctx)
+    links = tools.Map(tools.Delegate(ThroughLinks), ctx)
 
-    awards = Delegate(ThroughAwards, ctx)
-    venue = Delegate(ThroughVenues, ctx)
+    awards = tools.Map(tools.Delegate(ThroughAwards), ctx)
+    venues = tools.Map(tools.Delegate(ThroughVenues), ctx)
+    institutions = tools.Map(
+        tools.Delegate(Association.using(entity=tools.Delegate(Institution))), ctx
+    )
 
-    institutions = Map(Delegate(ThroughInstitutions), ctx.agency)
-    created = ctx.date
+    date_created = tools.ParseDate(ctx.date)
 
     class Extra:
         public_access_mandate = ctx.publicAccessMandate
