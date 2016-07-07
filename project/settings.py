aff657cb0d9a3448fe05aaf1d122509eb7fd5a60
Erin Braswell <erinspace@users.noreply.github.com>
2016-07-07 09:34:22 -0400

    Add SHARE-ized Django rest theme (#212)
    * Update templates dir for API for custom theme
    
    * Add template and static settings
    
    * Add some custom css and share image
    
    * Remove userlinks for now

--- project/settings.py
+++ project/settings.py
@@ -268,7 +268,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -356,6 +356,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(
+        os.path.dirname(__file__),
+        'static'
+    ),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL', 'http://localhost:9200/')
