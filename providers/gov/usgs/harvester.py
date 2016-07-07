6b71474d5c86b1a718e7ab13a1b0198d3e3aa1ea
Casey Rollins <cmr3rm@virginia.edu>
2016-07-07 16:56:15 -0400

    USGS harvester and normalizer.

--- providers/gov/usgs/harvester.py
+++ providers/gov/usgs/harvester.py
@@ -1,5 +1,33 @@
+from furl import furl
+
 from share.harvest.harvester import Harvester
 
 
 class USGSHarvester(Harvester):
-    pass
+    url = 'https://pubs.er.usgs.gov/pubs-services/publication'
+
+    def do_harvest(self, start_date, end_date):
+        end_date = end_date.date()
+        start_date = start_date.date()
+
+        days_back = (end_date - start_date).days
+
+        resp = self.requests.get(furl(self.url).set(query_params={
+            'mod_x_days': days_back,
+        }).url)
+
+        records = resp.json()['records']
+        page = 1
+
+        while records:
+            for record in records:
+                record_id = record['id']
+                yield(record_id, record)
+
+            page += 1
+            resp = self.requests.get(furl(self.url).set(query_params={
+                'mod_x_days': days_back,
+                'page_number': page,
+            }).url)
+
+            records = resp.json()['records']
---
+++ providers/gov/usgs/normalizer.py
@@ -0,0 +1,82 @@
+from share.normalize import *
+from share.normalize.utils import format_doi_as_url
+
+
+class Person(Parser):
+    given_name = Maybe(ctx, 'given')
+    family_name = ctx.family
+
+
+class Contributor(Parser):
+    order_cited = ctx('index')
+    cited_name = Join(Concat(Maybe(ctx, 'given'), ctx.family), ' ')
+    person = Delegate(Person, ctx)
+
+
+class Link(Parser):
+    url = ctx
+    type = RunPython('get_link_type', ctx)
+
+    def get_link_type(self, link):
+        if 'dx.doi.org' in link:
+            return 'doi'
+        elif 'usgs.gov' in link:
+            return 'provider'
+        return 'misc'
+
+
+class ThroughLinks(Parser):
+    link = Delegate(Link, ctx)
+
+
+class Publisher(Parser):
+    name = ctx
+
+
+class Association(Parser):
+    pass
+
+
+class CreativeWork(Parser):
+
+    def format_doi_as_url(self, doi):
+        return format_doi_as_url(self, doi)
+
+    def format_usgs_id_as_url(self, id):
+        return 'https://pubs.er.usgs.gov/publication/{}'.format(id)
+
+    def get_links(self, links):
+        for link in links:
+            return link.get('url')
+
+    title = ctx.title
+    description = Maybe(ctx, 'docAbstract')
+    date_updated = ParseDate(ctx.lastModifiedDate)
+    date_published = ParseDate(ctx.displayToPublicDate)
+    language = Maybe(ctx, 'language')
+    publishers = Map(Delegate(Association.using(entity=Delegate(Publisher))), Maybe(ctx, 'publisher'))
+    contributors = Map(Delegate(Contributor), Maybe(ctx, 'contributors').authors)
+    links = Map(
+        Delegate(ThroughLinks),
+        RunPython('format_doi_as_url', Maybe(ctx, 'doi')),
+        RunPython('get_links', ctx.links),
+        RunPython('format_usgs_id_as_url', ctx.id)
+    )
+
+    class Extra:
+        additional_online_files = Maybe(ctx, 'additionalOnlineFiles')
+        country = Maybe(ctx, 'country')
+        defined_type = Maybe(ctx, 'defined_type')
+        end_page = Maybe(ctx, 'endPage')
+        geographic_extents = Maybe(ctx, 'geographicExtents')
+        index_id = Maybe(ctx, 'indexId')
+        ipds_id = Maybe(ctx, 'ipdsId')
+        issue = Maybe(ctx, 'issue')
+        online_only = Maybe(ctx, 'onlineOnly')
+        other_geospatial = Maybe(ctx, 'otherGeospatial')
+        publication_subtype = Maybe(ctx, 'publicationSubtype')
+        publication_year = Maybe(ctx, 'publicationYear')
+        start_page = Maybe(ctx, 'startPage')
+        state = Maybe(ctx, 'state')
+        type = Maybe(ctx, 'type')
+        volume = Maybe(ctx, 'volume')
