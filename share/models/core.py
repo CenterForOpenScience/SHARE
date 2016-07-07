f53492064d594e11bfde418937a876e4ad83e8c8
erinspace <erin.braswell@gmail.com>
2016-07-06 21:12:30 -0400

    Allow null again for provider name and long title

--- share/models/core.py
+++ share/models/core.py
@@ -110,8 +110,8 @@ class ShareUser(AbstractBaseUser, PermissionsMixin):
     )
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
     robot = models.CharField(max_length=40, blank=True)
-    long_title = models.CharField(max_length=100, blank=True)
-    home_page = models.URLField(blank=True)
+    long_title = models.CharField(max_length=100, blank=True, null=True, default='')
+    home_page = models.URLField(blank=True, null=True, default='')
 
     def get_short_name(self):
         return self.robot if self.is_robot else self.username
