7bf9e87f1f5f1b684aebb7eddc08934cef4184ba
Casey Rollins <cmr3rm@virginia.edu>
2016-07-07 15:12:07 -0400

    Add harvester and normalizer.

--- providers/org/neurovault/harvester.py
+++ providers/org/neurovault/harvester.py
@@ -2,4 +2,15 @@ from share.harvest.harvester import Harvester
 
 
 class NeuroVaultHarvester(Harvester):
-    pass
+    url = 'http://www.neurovault.org/api/collections/?format=json'
+
+    def do_harvest(self, start_date, end_date):
+        api_url = self.url
+        while api_url:
+            response = self.requests.get(api_url)
+            records = response.json()
+            for record in records['results']:
+                record_id = record['id']
+                yield(record_id, record)
+
+            api_url = records['next']
