6ccf8ad4fde80688971dc87481922940605a5b54
Casey Rollins <cmr3rm@virginia.edu>
2016-07-07 21:19:21 -0400

    Add harvester and normalizer.

--- providers/ca/lwbin/harvester.py
+++ providers/ca/lwbin/harvester.py
@@ -2,4 +2,13 @@ from share.harvest.harvester import Harvester
 
 
 class LWBINHarvester(Harvester):
-    pass
+    # LWBIN does not have an HTTPS URL
+    url = 'http://130.179.67.140/api/3/action/current_package_list_with_resources'
+
+    def do_harvest(self, start_date, end_date):
+        # Searching by time is not permitted by the LWBIN CKAN API. All records must be scanned each time.
+        response = self.requests.get(self.url)
+        records = response.json()['result']
+        for record in records:
+            record_id = record['id']
+            yield(record_id, record)
