3688b6accdde019bc557868c37d97f5565b7ca5d
Casey Rollins <cmr3rm@virginia.edu>
2016-07-08 16:06:43 -0400

    Update NODC to be an OAI provider.

--- providers/gov/nodc/apps.py
+++ providers/gov/nodc/apps.py
@@ -1,11 +1,10 @@
-from share.provider import ProviderAppConfig
-from .harvester import NODCHarvester
+from share.provider import OAIProviderAppConfig
 
 
-class AppConfig(ProviderAppConfig):
+class AppConfig(OAIProviderAppConfig):
     name = 'providers.gov.nodc'
     version = '0.0.1'
     title = 'nodc'
     long_title = 'National Oceanographic Data Center'
     home_page = 'https://www.nodc.noaa.gov/'
-    harvester = NODCHarvester
+    url = 'https://data.nodc.noaa.gov/cgi-bin/oai-pmh?verb=ListRecords&metadataPrefix=oai_dc'
