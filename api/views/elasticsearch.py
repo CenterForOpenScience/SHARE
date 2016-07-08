e4ed8d0c8625d53dabef9acb5bdf897bf5b47797
Lauren Barker <lauren@cos.io>
2016-07-08 12:00:57 -0400

    add csrf_exempt decorator to elastic post

--- api/views/elasticsearch.py
+++ api/views/elasticsearch.py
@@ -1,6 +1,8 @@
 import requests
 from rest_framework import views, permissions
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from furl import furl
 from rest_framework.response import Response
 
@@ -16,6 +18,7 @@ class ElasticSearchView(views.APIView):
         resp = requests.get(es_url)
         return Response(data=resp.json(), headers={'Content-Type': 'application/json'})
 
+    @method_decorator(csrf_exempt)
     def post(self, request, *args, url_bits='', **kwargs):
         es_url = furl(settings.ELASTICSEARCH_URL).add(
             path=settings.ELASTICSEARCH_INDEX,
