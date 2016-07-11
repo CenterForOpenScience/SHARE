2c000c6e2d6c4704ddd0751911640f0cb0f2899f
Chris Wisecarver <cwisecarver@cos.io>
2016-07-10 20:17:06 -0400

    Moving csrf thingy to urls file

--- api/views/elasticsearch.py
+++ api/views/elasticsearch.py
@@ -1,8 +1,6 @@
 import requests
-from rest_framework import views, permissions
+from rest_framework import views
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 from furl import furl
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -19,7 +17,6 @@ class ElasticSearchView(views.APIView):
         resp = requests.get(es_url)
         return Response(data=resp.json(), headers={'Content-Type': 'application/json'})
 
-    @method_decorator(csrf_exempt)
     def post(self, request, *args, url_bits='', **kwargs):
         es_url = furl(settings.ELASTICSEARCH_URL).add(
             path=settings.ELASTICSEARCH_INDEX,
