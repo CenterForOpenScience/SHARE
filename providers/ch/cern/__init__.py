32ac93a7d64d004dd5faec5e3b64209162fc8242
Daniel Brown <dsb9ef@virginia.edu>
2016-07-11 15:23:28 -0400

    Add example record to __init__.py for CERN

--- providers/ch/cern/__init__.py
+++ providers/ch/cern/__init__.py
@@ -1,3 +1,59 @@
 default_app_config = 'providers.ch.cern.apps.AppConfig'
 
-
+"""<record>
+        <header>
+            <identifier>
+                oai:cds.cern.ch:2198005
+            </identifier>
+            <datestamp>
+                2016-07-10T22:00:40Z
+            </datestamp>
+            <setSpec>
+            cerncds:CERN
+            </setSpec>
+        </header>
+        <metadata>
+            <oai_dc:dc xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai
+            _dc/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"xsi:schemaLocation="http://www.openarchives.org/O
+            AI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+            <dc:identifier>
+                http://cds.cern.ch/record/2198005
+            </dc:identifier>
+            <dc:language>
+                eng
+            </dc:language>
+            <dc:creator>
+                CMS Collaboration
+            </dc:creator>
+            <dc:title>
+                Observation of the decay $ \mathrm{B}^{+} \to \psi ( 2 \mathrm{S} ) \phi ( 1020 ) \mathrm{ K }^{+} $ in
+                pp collisions at $\sqrt{s} = $ 8  TeV
+            </dc:title>
+            <dc:subject>
+                Particle Physics - Experiment
+            </dc:subject>
+            <dc:description>
+                The decay $ \mathrm{B}^{+} \to \psi( 2 \mathrm{S} ) \phi(1020) \mathrm{ K }^{+} $ is observed for the fi
+                rst time using data collected from pp collisions at $\sqrt{s} = $ 8  TeV by the CMS experiment at the LH
+                C, corresponding to an integrated luminosity of 19.6 fb$^{-1}$. The branching fraction of this decay is
+                measured, using the mode $\mathrm{B}^{+}\to \psi( 2 \mathrm{S} ) \mathrm{ K }^{+}$ as normalization, to
+                be ( 4.0 $\pm$ 0.4  (stat) $\pm$ 0.6  (syst) $\pm$ 0.2  ($\mathcal{B})$ )$\times$ 10$^{-6}$, where the t
+                hird uncertainty is from the imprecision in the normalization channel.
+            </dc:description>
+        <dc:publisher/>
+        <dc:identifier>
+            CERN-EP-2016-161
+        </dc:identifier>
+        <dc:identifier>
+            CMS-BPH-13-009-003
+        </dc:identifier>
+        <dc:identifier>
+            oai:cds.cern.ch:2198005
+        </dc:identifier>
+        <dc:date>
+            2016-07-10
+        </dc:date>
+    </oai_dc:dc>
+</metadata>
+</record>
+"""
