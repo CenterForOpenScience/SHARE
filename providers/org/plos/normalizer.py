b59a4d77a9e4e7141ac2ecfb4b7060fffc2507c4
Casey Rollins <cmr3rm@virginia.edu>
2016-07-11 11:24:51 -0400

    Add Extra class to PLOS normalizer.
    - Normalize publisher information.
    - Normalize DOI as a link.

--- providers/org/plos/normalizer.py
+++ providers/org/plos/normalizer.py
@@ -1,4 +1,5 @@
 from share.normalize import *  # noqa
+from share.normalize.utils import format_doi_as_url
 
 
 class Person(Parser):
@@ -14,12 +15,48 @@ class Contributor(Parser):
     order_cited = ctx('index')
 
 
+class Link(Parser):
+    url = ctx
+    type = Static('doi')
+
+
+class ThroughLinks(Parser):
+    link = Delegate(Link, ctx)
+
+
+class Publisher(Parser):
+    name = ctx
+
+
+class Association(Parser):
+    entity = Delegate(Publisher, ctx)
+
+
 class Publication(Parser):
+
+    def format_doi_as_url(self, doi):
+        return format_doi_as_url(self, doi)
+
     title = XPath(ctx, "str[@name='title_display']").str['#text']
     description = XPath(ctx, "arr[@name='abstract']/str").str
     contributors = Map(
         Delegate(Contributor),
         XPath(ctx, "arr[@name='author_display']").arr.str
     )
+    publishers = Map(
+        Delegate(Association),
+        XPath(ctx, "str[@name='journal']").str['#text']
+    )
+    links = Map(
+        Delegate(ThroughLinks),
+        RunPython(
+            'format_doi_as_url',
+            XPath(ctx, "str[@name='id']").str['#text']
+        )
+    )
     date_published = ParseDate(XPath(ctx, "date[@name='publication_date']").date['#text'])
-    # doi = ctx.xpath("str[@name='id']").str['#text']
+
+    class Extra:
+        eissn = XPath(ctx, "str[@name='eissn']").str['#text']
+        article_type = XPath(ctx, "str[@name='article_type']").str['#text']
+        score = XPath(ctx, "float[@name='score']").float['#text']
