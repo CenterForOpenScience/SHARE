fd705bc61b4543a3faa6c29b2f91c5b8b52c19ae
Henrique Moco <hmoco94@vt.edu>
2016-07-11 13:44:42 -0400

    changeset docs and markdown docs

--- api/views/workflow.py
+++ api/views/workflow.py
@@ -30,8 +30,54 @@ class ProviderViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class NormalizedDataViewSet(viewsets.ModelViewSet):
-    """
-    View showing all normalized data in the SHARE Dataset.
+    """View showing all normalized data in the SHARE Dataset.
+
+    ## Submit Changesets
+
+    Create
+
+        Method:        POST
+        Body (JSON):   {
+                        '@type': <type of document, exp: person>,
+                        '@id': <id if available, else _:<2 digit random int>>,
+                        <attribute_name>: <value>
+                        <relationship_name>: {
+                            '@type': <type>,
+                            '@id': <id>
+                        }
+                       }
+        Success:       200 OK
+
+    Update
+
+        Method:        POST
+        Body (JSON):   {
+                        '@type': <type of document, exp: person>,
+                        '@id': <id>,
+                        <attribute_name>: <value>,
+                        <relationship_name>: {
+                            '@type': <type>,
+                            '@id': <id>
+                        }
+                       }
+        Success:       200 OK
+
+    Merge
+
+        Method:        POST
+        Body (JSON):   {
+                        '@type': 'mergeAction',
+                        '@id': <_:<2 digit random int>>,
+                        'into': {
+                            '@type': <type of document>,
+                            '@id': <doc id>
+                        },
+                        'from': {
+                            '@type': <same type of document>,
+                            '@id': <doc id>
+                        }
+                       }
+        Success:       200 OK
     """
     permission_classes = [ReadOnlyOrTokenHasScopeOrIsAuthenticated, ]
     serializer_class = NormalizedDataSerializer
@@ -57,6 +103,15 @@ class ChangeSetViewSet(viewsets.ModelViewSet):
     ChangeSets are items that have been added to the SHARE dataset but may not yet have been accepted.
 
     These can come from harvesters and normalizers or from the curate interface.
+
+    ## Get Info
+
+        Method:        GET
+        Query Params:  `submitted_by=<Int>` -- share user that submitted the changeset
+        Success:       200 OK
+
+    ## Submit changes
+        Look at `/api/normalizeddata/`
     """
     serializer_class = ChangeSetSerializer
     # TODO: Add in scopes once we figure out who, why, and how.
