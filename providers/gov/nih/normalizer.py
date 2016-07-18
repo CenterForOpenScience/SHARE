5ac8930e2abbcb337deca748b9e454cf708ad7a3
Casey Rollins <cmr3rm@virginia.edu>
2016-07-18 15:54:17 -0400

    NIH normalizer updates for funders and awards.

--- providers/gov/nih/normalizer.py
+++ providers/gov/nih/normalizer.py
@@ -1,3 +1,5 @@
+import re
+
 from share.normalize import *
 
 
@@ -31,6 +33,176 @@ class ThroughTags(Parser):
     tag = Delegate(Tag, ctx)
 
 
+class Award(Parser):
+
+    def get_award_amount(self, award_info):
+        award = award_info.split('\n')[0]
+        award_amount = award.split(':')[1]
+        amount = re.findall(r'\d+', award_amount)
+        return amount[0]
+
+    def get_award_type(self, award_info):
+        award_type = award_info.split('\n')[1]
+        return award_type
+
+    # The amount of the award provided by the funding NIH Institute(s) or Center(s)
+    award = RunPython('get_award_amount', ctx)
+
+    # The funding mechanism for this award, i.e.
+    # one of the three main catgegories defined here: http://grants.nih.gov/grants/glossary.htm#Mechanism
+    description = RunPython('get_award_type', ctx)
+
+
+class ThroughAwards(Parser):
+    award = Delegate(Award, ctx)
+
+
+class Funder(Parser):
+
+    funding_ics_info = {
+        'CC': {
+            'full_name': 'Clinical Center',
+            'organizational_code': 'CC'
+        },
+        'CSR': {
+            'full_name': 'Center for Scientific Review',
+            'organizational_code': 'RG'
+        },
+        'CIT': {
+            'full_name': 'Center for Information Technology',
+            'organizational_code': 'CIT'
+        },
+        'FIC': {
+            'full_name': 'John E. Fogarty International Center',
+            'organizational_code': 'TW'
+        },
+        'NCATS': {
+            'full_name': 'National Center for Advancing Translational Sciences (NCATS)',
+            'organizational_code': 'TR'
+        },
+        'NCCIH': {
+            'full_name': 'National Center for Complementary and Integrative Health',
+            'organizational_code': 'AT'
+        },
+        'NCI': {
+            'full_name': 'National Cancer Institute',
+            'organizational_code': 'CA'
+        },
+        'NCRR': {
+            'full_name': 'National Center for Research Resources',
+            'organizational_code': 'RR'
+        },
+        'NEI': {
+            'full_name': 'National Eye Institute',
+            'organizational_code': 'EY'
+        },
+        'NHGRI': {
+            'full_name': 'National Human Genome Research Institute',
+            'organizational_code': 'HG'
+        },
+        'NHLBI': {
+            'full_name': 'National Heart, Lung, and Blood Institute',
+            'organizational_code': 'HL'
+        },
+        'NIA': {
+            'full_name': 'National Institute on Aging',
+            'organizational_code': 'AG'
+        },
+        'NIAAA': {
+            'full_name': 'National Institute on Alcohol Abuse and Alcoholism',
+            'organizational_code': 'AA'
+        },
+        'NIAID': {
+            'full_name': 'National Institute of Allergy and Infectious Diseases',
+            'organizational_code': 'AI'
+        },
+        'NIAMS': {
+            'full_name': 'National Institute of Arthritis and Musculoskeletal and Skin Diseases',
+            'organizational_code': 'AR'
+        },
+        'NIBIB': {
+            'full_name': 'National Institute of Biomedical Imaging and Bioengineering',
+            'organizational_code': 'EB'
+        },
+        'NICHD': {
+            'full_name': 'Eunice Kennedy Shriver National Institute of Child Health and Human Development',
+            'organizational_code': 'HD'
+        },
+        'NIDA': {
+            'full_name': 'National Institute on Drug Abuse',
+            'organizational_code': 'DA'
+        },
+        'NIDCD': {
+            'full_name': 'National Institute on Deafness and Other Communication Disorders',
+            'organizational_code': 'DC'
+        },
+        'NIDCR': {
+            'full_name': 'National Institute of Dental and Craniofacial Research',
+            'organizational_code': 'DE'
+        },
+        'NIDDK': {
+            'full_name': 'National Institute of Diabetes and Digestive and Kidney Diseases',
+            'organizational_code': 'DK'
+        },
+        'NIEHS': {
+            'full_name': 'National Institute of Environmental Health Sciences',
+            'organizational_code': 'ES'
+        },
+        'NIGMS': {
+            'full_name': 'National Institute of General Medical Sciences',
+            'organizational_code': 'GM'
+        },
+        'NIMH': {
+            'full_name': 'National Institute of Mental Health',
+            'organizational_code': 'MH'
+        },
+        'NIMHD': {
+            'full_name': 'National Institute on Minority Health and Health Disparities',
+            'organizational_code': 'MD'
+        },
+        'NINDS': {
+            'full_name': 'National Institute of Neurological Disorders and Stroke',
+            'organizational_code': 'NS'
+        },
+        'NINR': {
+            'full_name': 'National Institute of Nursing Research',
+            'organizational_code': 'NR'
+        },
+        'NLM': {
+            'full_name': 'National Library of Medicine',
+            'organizational_code': 'LM'
+        },
+        'OD': {
+            'full_name': 'Office of the Director',
+            'organizational_code': 'OD'
+        }
+    }
+
+    def get_funding_ic_full_name(self, funding_info):
+        ic_acronym = funding_info.split(':')[0]
+        ic_info = self.funding_ics_info.get(ic_acronym)
+        if ic_info:
+            return ic_info.get('full_name')
+        return ic_acronym
+
+    def get_funding_ic_identifier(self, funding_info):
+        ic_acronym = funding_info.split(':')[0]
+        ic_info = self.funding_ics_info.get(ic_acronym)
+        if ic_info:
+            return ic_info.get('organizational_code')
+        return ic_acronym
+
+    # The full name of the IC, as defined here: http://grants.nih.gov/grants/glossary.htm#InstituteorCenter(IC)
+    name = RunPython('get_funding_ic_full_name', ctx)
+
+    # The organizational code of the IC, as defined here: http://grants.nih.gov/grants/glossary.htm#InstituteorCenter(IC)
+    community_identifier = RunPython('get_funding_ic_identifier', ctx)
+
+
+class Association(Parser):
+    entity = Delegate(Funder, ctx)
+
+
 class CreativeWork(Parser):
 
     PROJECT_BASE_URL = 'https://projectreporter.nih.gov/project_info_description.cfm?aid={}'
@@ -56,6 +228,21 @@ class CreativeWork(Parser):
         Delegate(ThroughTags),
         Maybe(ctx.row.PROJECT_TERMSX, 'TERM')
     )
+    funders = Map(
+        Delegate(Association),
+        ctx.row.FUNDING_ICs
+    )
+    awards = Map(
+        Delegate(ThroughAwards),
+        Join(
+            Concat(
+                ctx.row.FUNDING_ICs,
+                ctx.row.FUNDING_MECHANISM
+            ),
+            '\n'
+        )
+    )
+
 
     class Extra:
         activity = ctx.row.ACTIVITY
@@ -71,8 +258,8 @@ class CreativeWork(Parser):
         fiscal_year = ctx.row.FY
         foa_number = ctx.row.FOA_NUMBER
         full_project_number = ctx.row.FULL_PROJECT_NUM
-        funding_ics = ctx.row.FUNDING_ICs
-        funding_mechanism = ctx.row.FUNDING_MECHANISM
+        # funding_ics = ctx.row.FUNDING_ICs
+        # funding_mechanism = ctx.row.FUNDING_MECHANISM
         ic_name = ctx.row.IC_NAME
         nih_spending_cats = ctx.row.NIH_SPENDING_CATS
         organization_city = ctx.row.ORG_CITY
