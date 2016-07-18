d41ba57ca4522afe5b66dd4f284fc8f3d5760043
Casey Rollins <cmr3rm@virginia.edu>
2016-07-18 10:33:02 -0400

    Update USGS harvester.

--- providers/gov/usgs/harvester.py
+++ providers/gov/usgs/harvester.py
@@ -1,5 +1,7 @@
 from furl import furl
 
+from datetime import date, timedelta
+
 from share.harvest.harvester import Harvester
 
 
@@ -7,27 +9,34 @@ class USGSHarvester(Harvester):
     url = 'https://pubs.er.usgs.gov/pubs-services/publication'
 
     def do_harvest(self, start_date, end_date):
+        today = date.today()
         end_date = end_date.date()
         start_date = start_date.date()
 
-        days_back = (end_date - start_date).days
+        end_days_back = (today - end_date).days
+        start_days_back = (today - start_date).days
 
-        resp = self.requests.get(furl(self.url).set(query_params={
-            'mod_x_days': days_back,
-        }).url)
-
-        records = resp.json()['records']
-        page = 1
-
-        while records:
-            for record in records:
-                record_id = record['id']
-                yield(record_id, record)
-
-            page += 1
+        # The USGS API does not support date ranges
+        for days_back in range(end_days_back, start_days_back):
+            page = 1
             resp = self.requests.get(furl(self.url).set(query_params={
-                'mod_x_days': days_back,
+                'mod_x_days': days_back + 1,
                 'page_number': page,
+                'page_size': 100
             }).url)
 
             records = resp.json()['records']
+
+            while records:
+                for record in records:
+                    record_id = record['id']
+                    yield (record_id, record)
+
+                page += 1
+                resp = self.requests.get(furl(self.url).set(query_params={
+                    'mod_x_days': days_back + 1,
+                    'page_number': page,
+                    'page_size': 100
+                }).url)
+
+                records = resp.json()['records']
