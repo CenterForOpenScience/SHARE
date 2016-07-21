83913f6d49b74b0b79a67e4cdbf2d30af361f984
Shaquille Johnson <SSJohns@users.noreply.github.com>
2016-07-21 15:45:54 -0400

    feature/file storage (#314)
    * changes for E-LIS normalizer
    
    * added language catch to only catch first language in list.
    
    * erased unneeded normalizer from code
    
    * fixed empty language problem
    
    * updated gitignore
    
    * migrations to the elis frame
    
    * fix
    
    * common with develop

--- project/settings.py
+++ project/settings.py
@@ -392,6 +392,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
