2aecfc72ab736f9a6425c2646b93921d853232a9
Shaquille Johnson <sjohns25@nd.edu>
2016-07-21 14:00:15 -0400

    changes to files

--- project/static.py
+++ project/static.py
@@ -1,13 +1,1 @@
-from .utils import root
 
-STATIC_ROOT = root('static_media', 'static_root')
-STATIC_URL = '/media/static/'
-
-STATICFILES_DIRS = (
-    root('static'),
-)
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
