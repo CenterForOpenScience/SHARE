1f5890922fe1abd69f5396d266074256f1a4b4a4
Chris Seto <chriskseto@gmail.com>
2016-07-28 14:59:11 -0400

    Wrap awardeeName in Try

--- providers/gov/nsfawards/normalizer.py
+++ providers/gov/nsfawards/normalizer.py
@@ -32,7 +32,7 @@ class Award(Parser):
     class Extra:
         awardee_city = ctx.awardeeCity
         funds_obligated_amt = ctx.fundsObligatedAmt
-        name = ctx.awardeeName
+        name = tools.Try(ctx.awardeeName)
         awardee_city = ctx.awardeeCity
         awardee_state_code = ctx.awardeeStateCode
 
@@ -42,7 +42,7 @@ class ThroughAwards(Parser):
 
 
 class Venue(Parser):
-    name = ctx.awardeeName
+    name = tools.Try(ctx.awardeeName)
     location = tools.Join(tools.Concat(ctx.awardeeCity, ctx.awardeeStateCode), joiner=', ')
 
     class Extra:
