e85372af6838a3a04c151e6a20ec632530ffaea0
Chris Seto <chriskseto@gmail.com>
2016-07-29 13:42:36 -0400

    Fix datacites namespaces

--- providers/org/datacite/apps.py
+++ providers/org/datacite/apps.py
@@ -10,3 +10,10 @@ class AppConfig(ProviderAppConfig):
     home_page = 'http://oai.datacite.org/'
     url = 'http://oai.datacite.org/oai'
     harvester = DataciteHarvester
+    namespaces = {
+        'http://purl.org/dc/elements/1.1/': 'dc',
+        'http://datacite.org/schema/kernel-3': None,
+        'http://www.openarchives.org/OAI/2.0/': None,
+        'http://schema.datacite.org/oai/oai-1.0/': None,
+        'http://www.openarchives.org/OAI/2.0/oai_dc/': None,
+    }
