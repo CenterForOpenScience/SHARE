74e7e5e6a2013c67af63bcdd2cd46f6ea6bdf459
Chris Seto <chriskseto@gmail.com>
2016-08-02 10:34:12 -0400

    Lock down elasticsearch

--- api/urls.py
+++ api/urls.py
@@ -37,5 +37,5 @@ router.register(r'providers', views.ProviderViewSet, base_name=views.ProviderVie
 
 urlpatterns = [
     url(r'userinfo/?', views.ShareUserView.as_view(), name='userinfo'),
-    url(r'search/(?P<url_bits>.*)', csrf_exempt(views.ElasticSearchView.as_view()), name='search'),
+    url(r'search/(?!.*_bulk\/?$)(?P<url_bits>.*)', csrf_exempt(views.ElasticSearchView.as_view()), name='search'),
 ] + router.urls
