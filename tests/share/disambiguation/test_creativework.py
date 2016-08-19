9cd6291fbefc466bc5bd4cf49e4ec635b621717b
Abram Booth <abram@cos.io>
2016-08-19 11:26:36 -0400

    Test cleanup

--- tests/share/disambiguation/test_creativework.py
+++ tests/share/disambiguation/test_creativework.py
@@ -3,19 +3,16 @@ import pytest
 from share.models import CreativeWork, Change, ChangeSet
 from share.disambiguation import disambiguate
 
-def create_work(attrs):
-    return CreativeWork.objects.create(**attrs)
-
 
 class TestAbstractWork:
 
     @pytest.mark.django_db
     def test_disambiguates(self, change_ids):
-        oldWork = create_work({
-            'title': 'all about giraffes',
-            'description': 'see here is the the thing about giraffes',
-            'change_id': change_ids.get()
-        })
+        oldWork = CreativeWork.objects.create(
+            title='all about giraffes',
+            description='see here is the the thing about giraffes',
+            change_id=change_ids.get()
+        )
         disWork = disambiguate('_:', {'title': 'all about giraffes'}, CreativeWork)
 
         assert disWork is not None
@@ -25,22 +22,22 @@ class TestAbstractWork:
 
     @pytest.mark.django_db
     def test_does_not_disambiguate(self, change_ids):
-        oldWork = create_work({
-            'title': 'all about giraffes',
-            'description': 'see here is the the thing about giraffes',
-            'change_id': change_ids.get()
-        })
+        oldWork = CreativeWork.objects.create(
+            title='all about giraffes',
+            description='see here is the the thing about giraffes',
+            change_id=change_ids.get()
+        )
         disWork = disambiguate('_:', {'title': 'all about short-necked ungulates'}, CreativeWork)
 
         assert disWork is None
 
     @pytest.mark.django_db
     def test_does_not_disambiguate_empty_string(self, change_ids):
-        oldWork = create_work({
-            'title': '',
-            'description': 'see here is the the thing about emptiness',
-            'change_id': change_ids.get()
-        })
+        oldWork = CreativeWork.objects.create(
+            title='',
+            description='see here is the the thing about emptiness',
+            change_id=change_ids.get()
+        )
         disWork = disambiguate('_:', {'title': ''}, CreativeWork)
 
         assert disWork is None
