64d2179ceff3964b02fd3f62612fe8d7a741d18d
Matthew Keitelman <zamattiac@gmail.com>
2016-08-25 11:25:36 -0400

    Add wisconsin directory

---
+++ providers/edu/wisconsin/__init__.py
@@ -0,0 +1,43 @@
+default_app_config = 'providers.edu.wisconsin.apps.AppConfig'
+
+"""
+Example record:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<record xmlns="http://www.openarchives.org/OAI/2.0/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <header>
+        <identifier>oai:minds.wisconsin.edu:1793/7098</identifier>
+        <datestamp>2016-05-26T18:11:35Z</datestamp>
+        <setSpec>hdl_1793_345</setSpec>
+    </header>
+    <metadata>
+        <oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+            <dc:creator>Henry, Darrell James, 1951-</dc:creator>
+            <dc:date>2007-03-21T19:39:45Z</dc:date>
+            <dc:date>2007-12-19T20:46:18Z</dc:date>
+            <dc:date>2007-03-21T19:39:45Z</dc:date>
+            <dc:date>2007-12-19T20:46:18Z</dc:date>
+            <dc:date>1976</dc:date>
+            <dc:identifier>ocm53789669</dc:identifier>
+            <dc:identifier>http://digital.library.wisc.edu/1793/7098</dc:identifier>
+            <dc:description>ix, 295 leaves : ill. ; 29 cm.</dc:description>
+            <dc:format>59933060 bytes</dc:format>
+            <dc:format>49 bytes</dc:format>
+            <dc:format>10459 bytes</dc:format>
+            <dc:format>691 bytes</dc:format>
+            <dc:format>2010 bytes</dc:format>
+            <dc:format>311 bytes</dc:format>
+            <dc:format>application/pdf</dc:format>
+            <dc:format>application/octet-stream</dc:format>
+            <dc:format>application/octet-stream</dc:format>
+            <dc:format>application/octet-stream</dc:format>
+            <dc:format>text/plain</dc:format>
+            <dc:format>text/plain</dc:format>
+            <dc:language>en</dc:language>
+            <dc:subject>Peridotite --Oregon</dc:subject>
+            <dc:title>Spinel peridotites of the Gold Beach area, Southwestern Oregon</dc:title>
+            <dc:type>Thesis</dc:type>
+        </oai_dc:dc>
+    </metadata>
+</record>
+"""
