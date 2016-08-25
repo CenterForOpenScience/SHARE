22ae520316f448cc153a1b7aeb37a8323d103460
Lauren Barker <lauren@cos.io>
2016-08-25 11:48:21 -0400

    Add v1 normalizer

---
+++ share/normalize/v1_push.py
@@ -0,0 +1,322 @@
+import logging
+
+from share.normalize import ctx, tools
+from share.normalize.parsers import Parser
+
+
+logger = logging.getLogger(__name__)
+
+
+class Link(Parser):
+
+    url = ctx
+    type = 'url'
+
+
+class ProviderLink(Parser):
+
+    schema = 'Link'
+
+    url = ctx
+    type = 'provider'
+
+
+class ThroughLinks(Parser):
+
+    link = tools.Delegate(Link, ctx)
+
+
+class ProviderThroughLinks(Parser):
+
+    link = tools.Delegate(ProviderLink, ctx)
+
+
+class Publisher(Parser):
+
+    name = ctx.name
+    url = tools.Join(tools.Try(ctx.sameAs))
+
+    class Extra:
+
+        publisher = ctx
+
+
+class Funder(Parser):
+
+    community_identifier = tools.Try(ctx.sponsorIdentifier)
+    name = ctx.sponsorName
+
+
+class ThroughAwards:
+
+    award = ctx
+
+
+class Award:
+
+    # award will become name
+    award = tools.Try(ctx.awardIdentifier)
+    description = ctx.awardName
+    url = tools.Try(ctx.awardIdentifier)
+
+
+class Institution(Parser):
+
+    name = ctx
+
+
+class Organization(Parser):
+
+    name = ctx
+
+
+class Association(Parser):
+    pass
+
+
+class Email(Parser):
+
+    email = ctx
+
+
+class PersonEmail(Parser):
+
+    email = ctx
+
+
+class Identifier(Parser):
+
+    url = ctx
+
+
+class ThroughIdentifiers(Parser):
+
+    identifier = tools.Delegate(Identifier, ctx)
+
+
+class Person(Parser):
+
+    suffix = tools.ParseName(ctx.name).suffix
+    family_name = tools.ParseName(ctx.name).last
+    given_name = tools.ParseName(ctx.name).first
+    additional_name = tools.ParseName(ctx.name).middle
+
+    emails = tools.Delegate(PersonEmail, tools.Try(ctx.email))
+    affiliations = tools.Map(
+        tools.Delegate(Association.using(entity=tools.Delegate(Organization))),
+        tools.Try(ctx.affiliation)
+    )
+
+    identifiers = tools.Map(
+        tools.Delegate(ThroughIdentifiers),
+        tools.Try(ctx.sameAs)
+    )
+
+    class Extra:
+
+        givenName = tools.Try(ctx.givenName)
+
+        familyName = tools.Try(ctx.familyName)
+
+        additonalName = tools.Try(ctx.additionalName)
+
+        name = tools.Try(ctx.name)
+
+
+class Contributor(Parser):
+
+    person = tools.Delegate(Person, ctx)
+    cited_name = ctx.name
+    order_cited = ctx('index')
+
+
+class Tag(Parser):
+
+    name = ctx
+
+
+class ThroughTags(Parser):
+
+    tag = tools.Delegate(Tag, ctx)
+
+
+class Subject(Parser):
+
+    name = ctx
+
+
+class ThroughSubjects(Parser):
+
+    subject = tools.Delegate(Subject, ctx)
+
+
+class CreativeWork(Parser):
+
+    ORGANIZATION_KEYWORDS = (
+        'the',
+        'center'
+    )
+    INSTITUTION_KEYWORDS = (
+        'school',
+        'university',
+        'institution',
+        'institute'
+    )
+
+    awards = tools.Map(
+        tools.Delegate(ThroughAwards),
+        tools.Try(ctx.sponsorships.award)
+    )
+
+    # unsure how to tell difference between person and org
+    contributors = tools.Map(
+        tools.Delegate(Contributor),
+        tools.RunPython(
+            'get_contributors',
+            tools.Try(ctx.contributors),
+            'contributor'
+        )
+    )
+
+    date_updated = tools.ParseDate(ctx.providerUpdatedDateTime)
+
+    description = ctx.description
+
+    funders = tools.Map(
+        tools.Delegate(Association.using(entity=tools.Delegate(Funder))),
+        tools.Try(ctx.sponsorships.sponsor)
+    )
+
+    institutions = tools.Map(
+        tools.Delegate(Association.using(entity=tools.Delegate(Institution))),
+        tools.RunPython(
+            'get_contributors',
+            tools.Try(ctx.contributors),
+            'institution'
+        )
+    )
+
+    # Note: this is only taking the first language in the case of multiple languages
+    language = tools.ParseLanguage(
+        tools.Try(ctx.languages[0]),
+    )
+
+    links = tools.Concat(
+        tools.Map(
+            tools.Delegate(ThroughLinks),
+            tools.Concat(
+                tools.Try(ctx.uris.canonicalUri),
+                tools.Try(ctx.uris.descriptorUris),
+                tools.Try(ctx.uris.objectUris)
+            )
+        ),
+        tools.Map(
+            tools.Delegate(ProviderThroughLinks),
+            tools.Concat(
+                tools.Try(ctx.uris.providerUris),
+            )
+        )
+    )
+
+    organizations = tools.Map(
+        tools.Delegate(Association.using(entity=tools.Delegate(Organization))),
+        tools.RunPython(
+            'get_contributors',
+            tools.Try(ctx.contributors),
+            'organization'
+        )
+    )
+
+    # unsure how to tell difference between person and org
+    publishers = tools.Map(
+        tools.Delegate(Association.using(entity=tools.Delegate(Publisher))),
+        tools.Try(ctx.publisher)
+    )
+
+    rights = tools.Join(tools.Try(ctx.licenses.uri))
+
+    # unsure if these should actually go in subjects
+    subjects = tools.Map(
+        tools.Delegate(ThroughSubjects),
+        tools.Try(ctx.subjects)
+    )
+
+    # unsure if to include both tags and subjects
+    tags = tools.Map(
+        tools.Delegate(ThroughTags),
+        tools.Concat(
+            tools.Try(ctx.tags),
+            tools.Try(ctx.subjects)
+        )
+    )
+
+    title = ctx.title
+
+    class Extra:
+        """
+        Fields that are combined in the base parser are relisted as singular elements that match
+        their original entry to preserve raw data structure.
+        """
+
+        freeToRead = tools.Try(ctx.freeToRead)
+
+        languages = tools.Try(ctx.languages)
+
+        licenses = tools.Try(ctx.licenses)
+
+        otherProperties = tools.Try(ctx.otherProperties)
+
+        publisher = tools.Try(ctx.publisher)
+
+        subjects = tools.Try(ctx.subjects)
+
+        sponsorships = tools.Try(ctx.sponsorships)
+
+        tags = tools.Try(ctx.tags)
+
+        uris = tools.Try(ctx.uris)
+
+        version = tools.Try(ctx.version)
+
+    def get_contributors(self, options, entity):
+        """
+        Returns list of organization, institutions, or contributors names based on entity type.
+        """
+        options = [o if isinstance(o, str) else o['#text'] for o in options]
+
+        if entity == 'organization':
+            organizations = [
+                value for value in options if
+                (
+                    value and
+                    not self.list_in_string(value, self.INSTITUTION_KEYWORDS) and
+                    self.list_in_string(value, self.ORGANIZATION_KEYWORDS)
+                )
+            ]
+            return organizations
+        elif entity == 'institution':
+            institutions = [
+                value for value in options if
+                (
+                    value and
+                    self.list_in_string(value, self.INSTITUTION_KEYWORDS)
+                )
+            ]
+            return institutions
+        elif entity == 'contributor':
+            people = [
+                value for value in options if
+                (
+                    value and
+                    not self.list_in_string(value, self.INSTITUTION_KEYWORDS) and not
+                    self.list_in_string(value, self.ORGANIZATION_KEYWORDS)
+                )
+            ]
+            return people
+        else:
+            return options
+
+    def list_in_string(self, string, list_):
+        if any(word in string.lower() for word in list_):
+            return True
+        return False
