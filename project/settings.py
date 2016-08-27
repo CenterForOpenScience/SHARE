0d9b16d6720a0f618d8833a3d3c7adffb937b005
Chris Seto <chriskseto@gmail.com>
2016-08-27 17:53:53 -0400

    Temporarily disable sentry

--- project/settings.py
+++ project/settings.py
@@ -341,10 +341,11 @@ if DEBUG:
     AUTH_PASSWORD_VALIDATORS = []
 # else:
 INSTALLED_APPS += [
-    'raven.contrib.django.raven_compat',
+    # 'raven.contrib.django.raven_compat',
 ]
 RAVEN_CONFIG = {
-    'dsn': os.environ.get('SENTRY_DSN', None),
+    # 'dsn': os.environ.get('SENTRY_DSN', None),
+    'dsn': None,
     'release': os.environ.get('GIT_COMMIT', None),
 }
 
