00b789b2669031e5514677d68033f17a1c93e60c
Chris Seto <chriskseto@gmail.com>
2016-08-27 12:28:27 -0400

    Update provider short names

--- providers/org/biorxiv/apps.py
+++ providers/org/biorxiv/apps.py
@@ -6,7 +6,7 @@ class AppConfig(ProviderAppConfig):
     name = 'providers.org.biorxiv'
     version = '0.0.0'
     title = 'biorxiv'
-    long_title = 'BioRxiv'
+    long_title = 'bioRxiv'
     home_page = 'http://biorxiv.org/'
     rate_limit = (1, 3)
     url = 'http://connect.biorxiv.org/biorxiv_xml.php?subject=all'
