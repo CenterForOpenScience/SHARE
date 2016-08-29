909cc07e1197523ee5028d5709c3aae5df2886c5
Chris Seto <chriskseto@gmail.com>
2016-08-29 02:18:29 -0400

    Re enable sentry

--- project/settings.py
+++ project/settings.py
@@ -341,11 +341,10 @@ if DEBUG:
     AUTH_PASSWORD_VALIDATORS = []
 # else:
 INSTALLED_APPS += [
-    # 'raven.contrib.django.raven_compat',
+    'raven.contrib.django.raven_compat',
 ]
 RAVEN_CONFIG = {
-    # 'dsn': os.environ.get('SENTRY_DSN', None),
-    'dsn': None,
+    'dsn': os.environ.get('SENTRY_DSN', None),
     'release': os.environ.get('GIT_COMMIT', None),
 }
 
