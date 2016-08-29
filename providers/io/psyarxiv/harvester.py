bd2519e894142c2e506dab1b542ae536ad0f2720
erinspace <erin.braswell@gmail.com>
2016-08-29 14:10:10 -0400

    Add psyarxiv harvester etc with temporary URL to get OSF meetings projects

---
+++ providers/io/psyarxiv/harvester.py
@@ -0,0 +1,31 @@
+import logging
+from typing import Tuple
+from typing import Union
+from typing import Iterator
+
+import arrow
+from furl import furl
+from django.conf import settings
+
+from providers.io.osf.harvester import OSFHarvester
+
+logger = logging.getLogger(__name__)
+
+
+class PsyarxivHarvester(OSFHarvester):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.url = '{}v2/preprint_providers/psyarxiv/preprints/'.format(settings.OSF_API_URL)
+        # temporary harvest URL until preprint branding is complete
+        self.url = '{}v2/nodes/?filter[tags]=psyarxiv'.format(settings.OSF_API_URL)
+
+    def do_harvest(self, start_date: arrow.Arrow, end_date: arrow.Arrow) -> Iterator[Tuple[str, Union[str, dict, bytes]]]:
+
+        url = furl(self.url)
+
+        url.args['page[size]'] = 100
+        url.args['filter[date_modified][gt]'] = start_date.date().isoformat()
+        url.args['filter[date_modified][lt]'] = end_date.date().isoformat()
+
+        return self.fetch_records(url)
