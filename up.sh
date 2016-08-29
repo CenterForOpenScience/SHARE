c4ab26cbd4332106fea2aefdf0ea946baa4f0244
Abram Booth <aaxelb@users.noreply.github.com>
2016-08-29 14:44:25 -0400

    Remove synonyms (#401)
    * Remove SubjectSynonyms!
    
    * Add migration to remove SubjectSynony

--- up.sh
+++ up.sh
@@ -34,7 +34,6 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata ./share/models/initial_data.yaml
 python manage.py addsubjects ./share/fixtures/subjects.json
-python manage.py addsynonyms --all
 
 if [ -n "$BACKUP" ]; then
     python manage.py loaddata share_rawdata.json.gz
