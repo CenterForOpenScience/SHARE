3620717f51d5d6d6491a08da1a3f5a343304ed2b
erinspace <erin.braswell@gmail.com>
2016-08-29 16:46:05 -0400

    Update hrvester to use args instead of overriding init for now

--- providers/io/psyarxiv/harvester.py
+++ providers/io/psyarxiv/harvester.py
@@ -16,15 +16,15 @@ class PsyarxivHarvester(OSFHarvester):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.url = '{}v2/preprint_providers/psyarxiv/preprints/'.format(settings.OSF_API_URL)
         # temporary harvest URL until preprint branding is complete
-        self.url = '{}v2/nodes/?filter[tags]=psyarxiv'.format(settings.OSF_API_URL)
+        # self.url = '{}v2/preprint_providers/psyarxiv/preprints/'.format(settings.OSF_API_URL)
 
     def do_harvest(self, start_date: arrow.Arrow, end_date: arrow.Arrow) -> Iterator[Tuple[str, Union[str, dict, bytes]]]:
 
         url = furl(self.url)
 
         url.args['page[size]'] = 100
+        url.args['filter[tags]'] = 'psyarxiv' # temporary - remove with proper preprint harvest
         url.args['embed'] = 'affiliated_institutions'  # temporary - remove with proper preprint harvest
         url.args['filter[date_modified][gt]'] = start_date.date().isoformat()
         url.args['filter[date_modified][lt]'] = end_date.date().isoformat()
--- providers/io/socarxiv/harvester.py
+++ providers/io/socarxiv/harvester.py
@@ -16,15 +16,15 @@ class SocarxivHarvester(OSFHarvester):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.url = '{}v2/preprint_providers/socarxiv/preprints/'.format(settings.OSF_API_URL)
         # temporary harvest URL until preprint branding is complete
-        self.url = '{}v2/nodes/?filter[tags]=socarxiv'.format(settings.OSF_API_URL)
+        # self.url = '{}v2/preprint_providers/socarxiv/preprints/'.format(settings.OSF_API_URL)
 
     def do_harvest(self, start_date: arrow.Arrow, end_date: arrow.Arrow) -> Iterator[Tuple[str, Union[str, dict, bytes]]]:
 
         url = furl(self.url)
 
         url.args['page[size]'] = 100
+        url.args['filter[tags]'] = 'socarxiv' # temporary - remove with proper preprint harvest
         url.args['embed'] = 'affiliated_institutions'  # temporary - remove with proper preprint harvest
         url.args['filter[date_modified][gt]'] = start_date.date().isoformat()
         url.args['filter[date_modified][lt]'] = end_date.date().isoformat()
