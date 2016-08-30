bfacef13e9e62e32d70ba7c0b7d3ba769839ed67
Matthew Keitelman <zamattiac@gmail.com>
2016-08-30 11:10:11 -0400

    Add example record

--- providers/edu/wm/__init__.py
+++ providers/edu/wm/__init__.py
@@ -3,4 +3,30 @@ default_app_config = 'providers.edu.wm.apps.AppConfig'
 """
 Example record:
 
+<?xml version="1.0"?>
+<record xmlns="http://www.openarchives.org/OAI/2.0/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <header>
+    <identifier>oai:publish.wm.edu:articles-1044</identifier>
+    <datestamp>2016-08-29T20:45:45Z</datestamp>
+    <setSpec>publication:music</setSpec>
+    <setSpec>publication:articles</setSpec>
+    <setSpec>publication:as</setSpec>
+    <setSpec>publication:musicpub</setSpec>
+  </header>
+  <metadata>
+    <oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+      <dc:title>The Qur'&#xE2;n in Indonesian Daily Life: The Public Project of Musical Oratory</dc:title>
+      <dc:creator>Rasmussen, Anne K.</dc:creator>
+      <dc:description>In Indonesia, amidst a plethora of unique Southeast Asian popular and folk music, Western music, and traditional gamelan styles, the recitation of the Qur'an pervades daily life as an archetype of Muslim authenticity. Removed by thousands of miles and hundreds of years from the source of Islam, Indonesians perform and experience the Qur'an in allegedly the same way as Muslims did during the time of the prophet, Muhammad. This article distills eight months of research as a student of professional male and female reciters of the Qur'an in Jakarta, Indonesia, the capital city of the country that is home to more Muslims than any other in the world. It is a work in progress that foregrounds issues pertaining to: historical process; music learning, conceptualization, and performance; the role of ritual specialists in a community; the social construction of gender roles; and the congruence of political ideology and religious practice in contemporary Indonesia.' How has the system of Arab musical modes that are employed for religious performance, been transmitted and perpetuated in Indonesia for nearly 400 years? What are the roles of professional male and female reciters (Qari' and Qari'ah) as they enable the continuity, through space and time, of this religious art? Why in Indonesia are women-school girls to national champions-participants in the public project of recitation when in the global Islamic umma (community) women's participation in religious life is often segregated and limited to the private domain? How has the project of New Order nationalism served to support and create social and cultural structures that officially institutionalize the recitation of the Qur'an, and how will the dynamic current of Reformasi (Reformation) effect this support? Each aspect of the investigation is seasoned with and limited by my own experiences as they were lived in the course of my work with those in Jakarta who practice seni baca al-Qur'an, the art of reciting the Qur'an.</dc:description>
+      <dc:date>2001-01-01T08:00:00Z</dc:date>
+      <dc:type>text</dc:type>
+      <dc:format>application/pdf</dc:format>
+      <dc:identifier>http://publish.wm.edu/articles/45</dc:identifier>
+      <dc:identifier>http://publish.wm.edu/cgi/viewcontent.cgi?article=1044&amp;amp;context=articles</dc:identifier>
+      <dc:source>Articles</dc:source>
+      <dc:publisher>W&amp;M Publish</dc:publisher>
+      <dc:subject>Ethnomusicology</dc:subject>
+    </oai_dc:dc>
+  </metadata>
+</record>
 """
