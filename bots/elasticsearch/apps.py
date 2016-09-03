50ec67209de7c805119b9725846afe6e3b8e24e8
Chris Seto <chriskseto@gmail.com>
2016-09-03 17:00:30 -0400

    Re enable indexing for other models

--- bots/elasticsearch/apps.py
+++ bots/elasticsearch/apps.py
@@ -14,7 +14,7 @@ class AppConfig(BotAppConfig):
     # Sources are also indexed as a special case
     INDEX_MODELS = [
         'AbstractCreativeWork',
-        # 'Entity',
-        # 'Person',
-        # 'Tag',
+        'Entity',
+        'Person',
+        'Tag',
     ]
