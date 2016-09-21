aa59ac9d1c3247be4f01db024b357cb087727d78
Chris Seto <chriskseto@gmail.com>
2016-09-21 11:26:53 -0400

    Correct ORCID validator

--- tests/share/normalize/test_links.py
+++ tests/share/normalize/test_links.py
@@ -6,6 +6,8 @@ from share.normalize.links import OrcidLink
 @pytest.mark.parametrize('orcid, result', [
     (None, TypeError('None is not of type str')),
     ('', ValueError(' cannot be expressed as an orcid')),
+    ('0000000317011251', 'https://orcid.org/0000-0003-1701-1251'),
+    ('0000000229129030', 'https://orcid.org/0000-0002-2912-9030'),
     ('0000000248692412', ValueError('0000000248692412 is not a valid ORCID. Failed checksum')),
     ('0000000248692419', 'https://orcid.org/0000-0002-4869-2419'),
     ('0000-0002-4869-2419', 'https://orcid.org/0000-0002-4869-2419'),
