2eb9725a5d20855b4ccff8e5ff7a58bdc6fb7638
Matthew Keitelman <zamattiac@gmail.com>
2016-10-06 12:14:59 -0400

    UCF

---
+++ providers/edu/ucf/apps.py
@@ -0,0 +1,9 @@
+from share.provider import OAIProviderAppConfig
+
+class AppConfig(OAIProviderAppConfig):
+    name = 'providers.edu.ucf'
+    version = '0.0.1'
+    title = 'ucf'
+    long_title = 'University of Central Florida'
+    home_page = 'http://stars.library.ucf.edu/'
+    url = 'http://stars.library.ucf.edu/do/oai/'
