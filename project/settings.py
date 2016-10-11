6356c7a696bf71b0f8ba24a85b74250f54e1cf5b
Matthew Keitelman <zamattiac@gmail.com>
2016-10-11 09:27:07 -0400

    Seafdec

--- project/settings.py
+++ project/settings.py
@@ -201,6 +201,7 @@ INSTALLED_APPS = [
     'providers.org.tdar',
     'providers.org.ucescholarship',
     'providers.org.zenodo',
+    'providers.ph.org.seafdec',
     'providers.pt.rcaap',
     'providers.ru.cyberleninka',
     'providers.tr.edu.hacettepe',
---
+++ providers/ph/org/seafdec/apps.py
@@ -0,0 +1,40 @@
+from share.provider import OAIProviderAppConfig
+
+class AppConfig(OAIProviderAppConfig):
+    name = 'providers.ph.org.seafdec'
+    version = '0.0.1'
+    title = 'seafdec'
+    long_title = 'SEAFDEC/AQD Institutional Repository'
+    home_page = 'http://conservancy.umn.edu/'
+    url = 'http://repository.seafdec.org.ph/oai/request'
+    approved_sets = [
+        'col_10862_26',
+        'col_10862_48',
+        'col_10862_49',
+        'col_10862_69',
+        'col_10862_81',
+        'com_10862_84',
+        'col_10862_151',
+        'col_10862_527',
+        'col_10862_534',
+        'col_10862_561',
+        'col_10862_658',
+        'col_10862_670',
+        'col_10862_683',
+        'col_10862_685',
+        'col_10862_702',
+        'col_10862_719',
+        'col_10862_741',
+        'col_10862_847',
+        'col_10862_948',
+        'col_10862_1053',
+        'col_10862_1482',
+        'col_10862_1662',
+        'col_10862_1666',
+        'col_10862_1667',
+        'col_10862_1699',
+        'col_10862_2143',
+        'col_10862_2271',
+        'col_10862_2273',
+        'col_10862_2818'
+    ]
