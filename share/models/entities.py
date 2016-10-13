278910ec9618e82381ad367d29a1297ffcc6f6f4
Abram Booth <abram@cos.io>
2016-10-13 12:08:26 -0400

    Use ModelGenerator for entities

--- share/models/entities.py
+++ share/models/entities.py
@@ -4,6 +4,8 @@ from share.models.base import ShareObject
 from share.models.base import TypedShareObjectMeta
 from share.models.fields import ShareManyToManyField
 
+from share.util import ModelGenerator
+
 __all__ = ('AbstractEntity', 'Person', 'Organization', 'Institution')
 
 
@@ -22,35 +24,5 @@ class AbstractEntity(ShareObject, metaclass=TypedShareObjectMeta):
         return self.name
 
 
-class Person(AbstractEntity):
-    family_name = models.TextField(blank=True, db_index=True)  # last
-    given_name = models.TextField(blank=True, db_index=True)  # first
-    additional_name = models.TextField(blank=True, db_index=True)  # can be used for middle
-    suffix = models.TextField(blank=True, db_index=True)
-
-    def __str__(self):
-        return self.get_full_name()
-
-    def get_full_name(self):
-        return ' '.join(x for x in [self.given_name, self.family_name, self.additional_name, self.suffix] if x)
-
-    def save(self, *args, **kwargs):
-        # TODO better way of reconciling Entity.name with Person.{family,given,additional}_name?
-        if not self.name:
-            self.name = self.get_full_name()
-        super(Person, self).save(*args, **kwargs)
-
-# TODO if Person.Meta is defined, system check complains "(models.E017) Proxy model 'Person' contains model fields."
-#    class Meta:
-#        verbose_name_plural = 'People'
-#        index_together = (
-#            ('family_name', 'given_name', 'additional_name', 'suffix')
-#        )
-
-
-class Organization(AbstractEntity):
-    pass
-
-
-class Institution(AbstractEntity):
-    pass
+generator = ModelGenerator(field_types={ 'text': models.TextField })
+globals().update(generator.generate_subclasses_from_yaml(__file__, AbstractEntity))
