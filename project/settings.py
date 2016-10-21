a648e92889a87816a3cc8de46f1e42c07473f136
Christina Maimone <christina.maimone@northwestern.edu>
2016-10-21 12:43:17 -0500

    update related to adding umd harvester; forgot settings file

--- project/settings.py
+++ project/settings.py
@@ -141,6 +141,7 @@ INSTALLED_APPS = [
     'providers.edu.ukansas',
     'providers.edu.uky',
     'providers.edu.umassmed',
+    'providers.edu.umd',
     'providers.edu.umich',
     'providers.edu.uncg',
     'providers.edu.unl_digitalcommons',
