f140a2c66fd8f5614f74e4853308967bcd9ed691
Chris Seto <chriskseto@gmail.com>
2016-11-03 15:02:58 -0400

    Correct rate limitting issues
      * last_called times are now set AFTER the function is called
      * AOI harvesters now respect 503s and Retry-After
      * Bump arxiv oai rate limit to 1/20 r/sec

--- share/harvest/harvester.py
+++ share/harvest/harvester.py
@@ -17,6 +17,40 @@ from django.utils.functional import cached_property
 logger = logging.getLogger(__name__)
 
 
+class RateLimittedProxy:
+
+    def __init__(self, proxy_to, calls, per_second):
+        self._proxy_to = proxy_to
+        self._allowance = calls
+        self._calls = calls
+        self._last_call = 0
+        self._per_second = per_second
+        self._cache = {}
+
+    def _check_limit(self):
+        if self._allowance > 1:
+            return
+        wait = self._per_second - (time.time() - self._last_call)
+        if wait > 0:
+            logger.debug('Rate limitting %s. Sleeping for %s', self._proxy_to, wait)
+            time.sleep(wait)
+        self._allowance = self._calls
+        logger.debug('Access granted for %s', self._proxy_to)
+
+    def _called(self):
+        self._allowance -= 1
+        self._last_call = time.time()
+
+    def __call__(self, *args, **kwargs):
+        self._check_limit()
+        ret = self._proxy_to(*args, **kwargs)
+        self._called()
+        return ret
+
+    def __getattr__(self, name):
+        return self._cache.setdefault(name, self.__class__(getattr(self._proxy_to, name), self._calls, self._per_second))
+
+
 class Harvester(metaclass=abc.ABCMeta):
 
     # TODO Make this apply across threads
@@ -27,18 +61,7 @@ class Harvester(metaclass=abc.ABCMeta):
         self.config = app_config
         self.rate_limit = getattr(self.config, 'rate_limit', self.rate_limit)
         self.allowance = self.rate_limit[0]
-
-    @property
-    def requests(self) -> requests:
-        if self.allowance < 1:
-            try:
-                time.sleep(self.rate_limit[1] - (time.time() - self.last_call))
-            except ValueError:
-                pass  # ValueError indicates a negative sleep time
-            self.allowance = self.rate_limit[0]
-        self.allowance -= 1
-        self.last_call = time.time()
-        return requests
+        self.requests = RateLimittedProxy(requests, *self.rate_limit)
 
     @cached_property
     def source(self):
