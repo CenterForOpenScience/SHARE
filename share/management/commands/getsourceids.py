d37e40158ae6aad8a3d6f9d717e58b3899ed00cb
Abram Booth <abram@cos.io>
2016-11-07 13:40:04 -0500

    Add app_label to getsourceids output

--- share/management/commands/getsourceids.py
+++ share/management/commands/getsourceids.py
@@ -8,13 +8,12 @@ from django.db import transaction
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('out_file', type=argparse.FileType('w'), help='Output file for json map of source ids')
         parser.add_argument('work_ids', nargs='*', type=int, help='The id(s) of the creative works to migrate')
 
-    def handle(self, out_file, work_ids, *args, **options):
+    def handle(self, work_ids, *args, **options):
 
         query = '''
-        SELECT DISTINCT acw.id, rd.provider_doc_id FROM
+        SELECT DISTINCT acw.id, rd.provider_doc_id, rd.app_label FROM
           share_rawdata AS rd
           JOIN share_normalizeddata AS nd ON rd.id = nd.raw_id
           JOIN share_changeset AS cs ON nd.id = cs.normalized_data_id
@@ -30,11 +29,11 @@ class Command(BaseCommand):
             source_ids = {}
             data = c.fetchone()
             while data:
-                (id, source_id) = data
+                (id, source_id, app_label) = data
                 try:
-                    source_ids[id].append(source_id)
+                    source_ids[id].append((app_label, source_id))
                 except KeyError:
-                    source_ids[id] = [source_id]
+                    source_ids[id] = [(app_label, source_id)]
                 data = c.fetchone()
 
-        json.dump(source_ids, out_file, indent=4)
+        print(json.dumps(source_ids, indent=4))
