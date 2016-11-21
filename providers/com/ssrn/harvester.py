ac21833efa45f9a59caad531e87f62e28801971b
Matthew Keitelman <zamattiac@gmail.com>
2016-11-21 16:43:48 -0500

    Use meta tags, run successful tests

--- providers/com/ssrn/harvester.py
+++ providers/com/ssrn/harvester.py
@@ -1,8 +1,9 @@
 import logging
 
-import pendulum
 from bs4 import BeautifulSoup
 from furl import furl
+import lxml
+import pendulum
 
 from share.harvest.harvester import Harvester
 
@@ -14,17 +15,17 @@ logger = logging.getLogger('__name__')
 
 class SSRNHarvester(Harvester):
     base_url = furl('https://papers.ssrn.com/sol3/JELJOUR_Results.cfm')
-    # codes = ['A0', 'A1', 'A2', 'A3', 'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'C0', 'C01', 'C02', 'C1', 'C2', 'C3', 'C4',
-    #          'C5', 'C6', 'C7', 'C8', 'C9', 'D0', 'D01', 'D02', 'D03', 'D04', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7',
-    #          'D8', 'D9', 'E0', 'E01', 'E02', 'E03', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'F0', 'F01', 'F02', 'F1', 'F2',
-    #          'F3', 'F4', 'F5', 'F6', 'G0', 'G01', 'G02', 'G1', 'G2', 'G3', 'H0', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6',
-    #          'H7', 'H8', 'I0', 'I1', 'I2', 'I3', 'J0', 'J01', 'J08', 'J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8',
-    #          'K0', 'K1', 'K2', 'K3', 'K4', 'L0', 'L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9', 'M0', 'M1', 'M2',
-    #          'M3', 'M4', 'M5', 'N0', 'N01', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9', 'O0', 'O1', 'O2',
-    #          'O3', 'O4', 'O5', 'P0', 'P1', 'P2', 'P3', 'P4', 'P5', 'Q0', 'Q01', 'Q02', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5',
-    #          'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8', 'Y9', 'Z0', 'Z1',
-    #          'Z2', 'Z3']
-    codes = ['C7']
+    # TODO: subject mapping with more specific codes
+    codes = ['A0', 'A1', 'A2', 'A3', 'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'C0', 'C01', 'C02', 'C1', 'C2', 'C3', 'C4',
+             'C5', 'C6', 'C7', 'C8', 'C9', 'D0', 'D01', 'D02', 'D03', 'D04', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7',
+             'D8', 'D9', 'E0', 'E01', 'E02', 'E03', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'F0', 'F01', 'F02', 'F1', 'F2',
+             'F3', 'F4', 'F5', 'F6', 'G0', 'G01', 'G02', 'G1', 'G2', 'G3', 'H0', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6',
+             'H7', 'H8', 'I0', 'I1', 'I2', 'I3', 'J0', 'J01', 'J08', 'J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8',
+             'K0', 'K1', 'K2', 'K3', 'K4', 'L0', 'L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9', 'M0', 'M1', 'M2',
+             'M3', 'M4', 'M5', 'N0', 'N01', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9', 'O0', 'O1', 'O2',
+             'O3', 'O4', 'O5', 'P0', 'P1', 'P2', 'P3', 'P4', 'P5', 'Q0', 'Q01', 'Q02', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5',
+             'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8', 'Y9', 'Z0', 'Z1',
+             'Z2', 'Z3']
 
     def do_harvest(self, start_date: pendulum.Pendulum, end_date: pendulum.Pendulum):
         return self.fetch_records(start_date, end_date)
@@ -45,7 +46,10 @@ class SSRNHarvester(Harvester):
                 print(urls)
                 for url in urls:
                     work = self.fetch_work('https://papers.ssrn.com/sol3/' + url)
-                    yield work['ID'], work
+                    work['code'] = code
+                    work['url'] = url.replace('papers.cfm?abstract_id=', '')
+                    work['id'] = url.replace('https://papers.ssrn.com/papers.cfm?abstract_id=', '')
+                    yield work['id'], work
                 if final_page:
                     break
 
@@ -77,40 +81,7 @@ class SSRNHarvester(Harvester):
 
     def fetch_work(self, url):
         r = self.requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        authors = soup.select('#innerWhite a h2')
-        dates = soup.select('.dateInfo > font')
-        data = {
-            'URL': url,
-            'ID': url.replace('https://papers.ssrn.com/sol3/papers.cfm?abstract_id=', ''),
-            'Title': soup.h1.text,
-            # 'Authors': {author.text: [author.parent['href'], author.parent.next_sibling.next_sibling ]
-            #            for author in authors},
-            'Journal': soup.select('#innerWhite i')[0].text if soup.select('#innerWhite i') else None,
-            'Abstract': soup.find(id='abstract').text,
-            'Date posted': dates[0].text,
-            'Date revised': dates[1].text if len(dates) > 1 else None,
-            'Citation': soup.select('#suggestedCitation .simField')[0].text,
-        }
-        # For page number and keywords:
-        for p in soup.select('p > font > b'):
-            data[p.text.replace(':', '')] = p.next_sibling
-        agents = {}
-        contacts = {}
-        for author in authors:
-            info = [author.text]
-            if isinstance(author.parent.next_sibling.next_sibling, str):
-                info.append(author.parent.next_sibling.next_sibling)
-            else:
-                info.append(author.parent.next_sibling.next_sibling.text)
-
-        for person in soup.select('.affiliationHold'):
-            print(person.h5.text)
-            print(person.div.text)
-        data['Contacts'] = contacts
-
+        soup = BeautifulSoup(r.text, 'lxml')
+        data = {tag['name']: tag['content'] for tag in soup.find_all('meta')[1:]}
+        data['citation_author'] = [tag['content'] for tag in soup.find_all('meta', {'name': 'citation_author'})]
         return data
-
-
-class NoPagesLeft(Exception):
-    pass
