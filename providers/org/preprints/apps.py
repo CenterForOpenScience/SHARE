396f1c37befc9715b179324195de633c8f1a5aff
Abram Booth <abram@cos.io>
2016-12-21 13:58:32 -0500

    Less ambiguous source name

--- providers/org/preprints/apps.py
+++ providers/org/preprints/apps.py
@@ -4,8 +4,8 @@ from share.provider import OAIProviderAppConfig
 class AppConfig(OAIProviderAppConfig):
     name = 'providers.org.preprints'
     version = '0.0.1'
-    title = 'Preprints'
-    long_title = 'Preprints'
+    title = 'Preprints.org'
+    long_title = 'Preprints.org'
     home_page = 'http://www.preprints.org'
     url = 'http://www.preprints.org/oaipmh/'
     time_granularity = False
