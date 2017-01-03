7e8e8186757384435bc93acc5b69e84f48e7d2c2
Abram Booth <abram@cos.io>
2017-01-03 14:12:22 -0500

    Add GWU example record

--- providers/edu/gwu/__init__.py
+++ providers/edu/gwu/__init__.py
@@ -1,1 +1,138 @@
 default_app_config = 'providers.edu.gwu.apps.AppConfig'
+
+""" Example record:
+<div class="col-xs-12 col-sm-8" itemscope="" itemtype="http://schema.org/CreativeWork">
+<h1 class="visibility">Planting the Seeds for Open Access Success: Growing Faculty Participation in a New Institutional Repository <span class="label label-success" title="Change the visibility of this resource">Open Access</span></h1>
+<p class="genericfile_description">GW Libraries is the library system for George Washington
+University, a private research university located in Washington DC. This
+poster will provide the roadmap of GW Libraries' initiative to promote open
+access among campus stakeholders. It will specifically highlight the
+library's outreach efforts to increase the participation in a new
+institutional repository (IR) of research faculty from the Graduate School of
+Education and Human Development.</p>
+<h2>Descriptions</h2>
+<dl class="dl-horizontal file-show-term file-show-descriptions">
+<dt>Author</dt>
+<dd><span itemprop="creator" itemscope="" itemtype="http://schema.org/Person">
+<span itemprop="name"><a href="/catalog?f%5Bcreator_sim%5D%5B%5D=Gooch%2C+Amanda">Gooch, Amanda</a></span>
+</span>
+<br/>
+<span itemprop="creator" itemscope="" itemtype="http://schema.org/Person">
+<span itemprop="name"><a href="/catalog?f%5Bcreator_sim%5D%5B%5D=Banks%2C+Dorinne">Banks, Dorinne</a></span>
+</span>
+<br/>
+</dd>
+<dt>GW Unit</dt>
+<dd> <a href="/catalog?f%5Bgw_affiliation_sim%5D%5B%5D=GW+Libraries">GW Libraries</a><br/>
+</dd>
+<dt>Date Created</dt>
+<dd>
+</dd>
+<dt>Type of Work</dt>
+<dd> <a href="/catalog?f%5Bresource_type_sim%5D%5B%5D=Poster">Poster</a><br/>
+</dd>
+<dt>Rights</dt>
+<dd> <a href="/catalog?rights=%22http%3A%2F%2Fcreativecommons.org%2Flicenses%2Fby-nc-nd%2F3.0%2Fus%2F%22&amp;search_field=advanced">Attribution-NonCommercial-NoDerivs 3.0 United States</a> <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/us/"><i class="glyphicon glyphicon-new-window"></i><br/></a><br/>
+<a href="/catalog?rights=%22None%22&amp;search_field=advanced">No Creative Commons rights have been assigned</a> <br/>
+</dd>
+<dt>Keyword</dt>
+<dd> <span itemprop="keywords"><a href="/catalog?f%5Btag_sim%5D%5B%5D=Open+access">Open access</a></span><br/>
+<span itemprop="keywords"><a href="/catalog?f%5Btag_sim%5D%5B%5D=Institutional+repository">Institutional repository</a></span><br/>
+<span itemprop="keywords"><a href="/catalog?f%5Btag_sim%5D%5B%5D=Faculty+participation">Faculty participation</a></span><br/>
+</dd>
+<dt>Identifier</dt>
+<dd>
+</dd>
+<dt>Contributor</dt>
+<dd>
+</dd>
+<dt>Publisher</dt>
+<dd>
+</dd>
+<dt>Language</dt>
+<dd> <span itemprop="inLanguage"><a href="/catalog?f%5Blanguage_sim%5D%5B%5D=English">English</a></span><br/>
+</dd>
+<dt>Location</dt>
+<dd>
+</dd>
+<dt>Related URL</dt>
+<dd>
+</dd>
+<dt>Previous Publication Information</dt>
+<dd></dd>
+<dt>Permanent Link</dt>
+<dd><a href="/files/s7526c435">https://scholarspace.library.gwu.edu/files/s7526c435</a></dd>
+</dl>
+<h2>File Details</h2>
+<dl class="dl-horizontal file-show-term file-show-details">
+<dt>Depositor</dt>
+<dd itemprop="accountablePerson" itemscope="" itemtype="http://schema.org/Person"><span itemprop="name"><a href="/users/sfl@gwu-dot-edu">Shira Eller</a></span></dd>
+<dt>Date Uploaded</dt>
+<dd itemprop="datePublished">2016-10-05T15:25:40+00:00</dd>
+<dt>Date Modified</dt>
+<dd itemprop="dateModified">2016-11-22T09:53:33-05:00</dd>
+<dt>Audit Status</dt>
+<dd>Audits have not yet been run on this file.</dd>
+<dt>Characterization</dt>
+<dd>
+<div>
+              File format: pdf (Portable Document Format)<br/>
+</div>
+<div>
+              Mime type: application/pdf<br/>
+</div>
+<div>
+                  File size: 451676
+                  <br/>
+</div>
+<div>
+                  Last modified: 2016:10:05 11:36:30-04:00
+                  <br/>
+</div>
+<div>
+                  Filename: Planting the Seeds for Open Access Success.pdf
+                  <br/>
+</div>
+<div>
+                  Original checksum: 0cbfe7d1c9f4e16d8a2df6ee02adaac3
+                  <br/>
+</div>
+<div>
+                  Well formed: true
+                  <br/>
+</div>
+<div>
+                  Valid: true
+                  <br/>
+</div>
+<div>
+                  File title: Planting the Seeds for Open Access Success: Growing Faculty Participation in a New Institutional Repository
+                  <br/>
+</div>
+<div>
+                  File author: Gooch, Amanda J.
+                  <br/>
+</div>
+<div>
+                  Page count: 1
+                  <br/>
+</div>
+<div>
+                  File language: en-US
+                  <br/>
+</div>
+</dd>
+</dl>
+<table class="table table-zebra-striped table-bordered" id="activity">
+<caption class="sr-only">Activity of users you follow</caption>
+<thead>
+<tr>
+<th>User Activity</th>
+<th>Date</th>
+</tr>
+</thead>
+<tbody>
+</tbody>
+</table>
+</div>
+"""
