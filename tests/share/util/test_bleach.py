33b20a4ed8b5680e07b78731fc1ce61d0814aa83
Lauren Barker <lauren@cos.io>
2017-01-05 10:03:28 -0500

    [SHARE-470][Test] Add style and class bleach test (#555)

--- tests/share/util/test_bleach.py
+++ tests/share/util/test_bleach.py
@@ -8,6 +8,7 @@ from project.settings import ALLOWED_TAGS
     ('<p class="Something-that-may-or-may-not-exist" style="size: large;">Horribly styled text</p>', 'Horribly styled text'),
     ('<a href="http://MyEvilSite.io/">Click me for puppies</a>', 'Click me for puppies'),
     ('<b>Puppies</b>', '<b>Puppies</b>'),
+    ('<i style="size: xxl; color: orange;" class="geocities-i" data-bind="ko: bestCities">GeoCities was best cities</i>', '<i>GeoCities was best cities</i>')
 ])
 def test_bleach(raw_html, bleached_html):
     bleached = bleach.clean(raw_html, strip=True, tags=ALLOWED_TAGS)
