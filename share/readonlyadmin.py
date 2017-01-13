3fa80e5054cbff2ec4de28c7d5aec107c42fb80c
Lauren Barker <lauren@cos.io>
2017-01-13 13:34:52 -0500

    Remove empty docstrings
    Don't wrap short lines
    
    Use single quotes

--- share/readonlyadmin.py
+++ share/readonlyadmin.py
@@ -1,26 +1,14 @@
-## adopted from http://mike.hostetlerhome.com/blog/2012/11/13/add-a-read-only-role-to-django-admin/
 from django.contrib import admin
 from django.core.exceptions import PermissionDenied
 
 
+## adopted from http://mike.hostetlerhome.com/blog/2012/11/13/add-a-read-only-role-to-django-admin/
 class ReadOnlyAdmin(admin.ModelAdmin):
-    """
-    """
 
     def has_add_permission(self, request, obj=None):
-        """
-        Arguments:
-        - `request`:
-        - `obj`:
-        """
         return self.__user_is_readonly(request)
 
     def has_delete_permission(self, request, obj=None):
-        """
-        Arguments:
-        - `request`:
-        - `obj`:
-        """
         return self.__user_is_readonly(request)
 
     def get_actions(self, request):
@@ -40,17 +28,15 @@ class ReadOnlyAdmin(admin.ModelAdmin):
             self.inlines = self.user_readonly_inlines
 
         try:
-            return super(ReadOnlyAdmin, self).change_view(
-                request, object_id, extra_context=extra_context)
+            return super(ReadOnlyAdmin, self).change_view(request, object_id, extra_context=extra_context)
         except PermissionDenied:
             pass
         if request.method == 'POST':
             raise PermissionDenied
         request.readonly = True
-        return super(ReadOnlyAdmin, self).change_view(
-            request, object_id, extra_context=extra_context)
+        return super(ReadOnlyAdmin, self).change_view(request, object_id, extra_context=extra_context)
 
     def __user_is_readonly(self, request):
         groups = [x.name for x in request.user.groups.all()]
 
-        return "readonly" in groups
+        return 'readonly' in groups
