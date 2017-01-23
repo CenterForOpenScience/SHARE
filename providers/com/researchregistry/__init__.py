fa5f4e80ccb8dfc87ae084a1f60977ad45321267
Abram Booth <abram@cos.io>
2017-01-23 11:38:02 -0500

    Add example record

--- providers/com/researchregistry/__init__.py
+++ providers/com/researchregistry/__init__.py
@@ -1,1 +1,63 @@
 default_app_config = 'providers.com.researchregistry.apps.AppConfig'
+
+"""
+Example Record:
+
+{
+    "field_10": "The systematic review has been performed in Greece.",
+    "field_11": "Morbid obesity",
+    "field_12": "single port Roux en Y gastric bypass",
+    "field_13": "Original studies reporting on outcomes following treatment of obesity and its complications with SILS-RYGB were considered eligible for this systematic review. The selection process excluded studies reporting on: 1) animal models, 2) patients treated with more than three incisions, 3) other bariatric operations than Roux-en-Y gastric bypass. Articles in language other than English were excluded from the study. ",
+    "field_14": "Systematic Review",
+    "field_15": "",
+    "field_16": "199",
+    "field_17": "199",
+    "field_18": "It has been performed only tabulation of the existing data without further statistical analysis.",
+    "field_19": "Single port RnY gastric bypass is a feasible procedure with improved cosmetic results, when compared to conventional techniques. However, it is accompanied with a higher complication rate.",
+    "field_2": {
+        "am_pm": "AM",
+        "date": "02/10/2015",
+        "date_formatted": "02/10/2015",
+        "hours": "12",
+        "minutes": "00",
+        "time": 720,
+        "timestamp": "02/10/2015 12:00 am",
+        "unix_timestamp": 1423526400000
+    },
+    "field_21": "researchregistry10",
+    "field_28": "",
+    "field_29": "",
+    "field_3": {
+        "first": "Ilias",
+        "last": "Doulamis"
+    },
+    "field_30": {
+        "url": "None"
+    },
+    "field_37": "",
+    "field_4": {
+        "first": "Konstantinos",
+        "last": "Economopoulos"
+    },
+    "field_5": "ledoul@hotmail.com",
+    "field_62": "",
+    "field_63": "This study is a systematic review and it does not involve human subjects.",
+    "field_64": "",
+    "field_66": [],
+    "field_68": "",
+    "field_7": "Transumbilical Roux-en-Y gastric bypass in morbidly obese patients: a systematic review",
+    "field_70": "",
+    "field_72": "",
+    "field_73": "",
+    "field_75": "",
+    "field_78": "",
+    "field_79": "",
+    "field_80": "",
+    "field_81": "",
+    "field_89": "",
+    "field_9": "None",
+    "field_94": "",
+    "field_97": "",
+    "id": "54da5085df11855c0e940398"
+}
+"""
