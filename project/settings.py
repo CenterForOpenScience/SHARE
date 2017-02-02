ac4a951660e214d4534f6096849e366c1e120711
Abram Booth <aaxelb@users.noreply.github.com>
2017-02-02 09:44:40 -0500

    [SHARE-507][Feature] researchregistry.com harvester/normalizer (#568)
    * Add com.researchregistry harvester
    
    * com.researchregistry normalizer
    
    * Fix harvester
    
    * Move favicon to the right place
    
    * Add example record
    
    * Plac8 flake8
    
    * Move researchregistry.com API id/keys to settings
    
    * Add linkable identifier.

--- project/settings.py
+++ project/settings.py
@@ -95,6 +95,7 @@ INSTALLED_APPS = [
     'providers.com.peerj',
     'providers.com.peerj.preprints',
     'providers.com.peerj.xml',
+    'providers.com.researchregistry',
     'providers.com.springer',
     'providers.edu.ageconsearch',
     'providers.edu.asu',
@@ -605,6 +606,8 @@ ALLOWED_TAGS = ['abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', '
 DATAVERSE_API_KEY = os.environ.get('DATAVERSE_API_KEY')
 PLOS_API_KEY = os.environ.get('PLOS_API_KEY')
 SPRINGER_API_KEY = os.environ.get('SPRINGER_API_KEY')
+RESEARCHREGISTRY_APPLICATION_ID = os.environ.get('RESEARCHREGISTRY_APPLICATION_ID', '54a1ac1032e4beb07e04ac2c')
+RESEARCHREGISTRY_API_KEY = os.environ.get('RESEARCHREGISTRY_API_KEY', 'renderer')
 
 import djcelery  # noqa
 djcelery.setup_loader()
