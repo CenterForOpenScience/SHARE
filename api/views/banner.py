d3d505c2c96d144fb66059a4a4dc0695234a1142
Chris Seto <chriskseto@gmail.com>
2017-03-08 14:12:08 -0500

    [SHARE-   ][Fix] Use exact counts for site banners

--- api/views/banner.py
+++ api/views/banner.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework_json_api.pagination import PageNumberPagination
 
 from api.serializers import SiteBannerSerializer
 from share.models import SiteBanner
@@ -7,6 +8,7 @@ from share.models import SiteBanner
 class SiteBannerViewSet(viewsets.ReadOnlyModelViewSet):
     """View showing all active site-wide announcements."""
     serializer_class = SiteBannerSerializer
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         return SiteBanner.objects.filter(active=True)
