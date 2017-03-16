0cc3493eef14abf2af4cf1454e2f773e86b86b8f
Abram Booth <abram@cos.io>
2017-03-16 11:27:21 -0400

    Add Cyprus University of Technology OAI provider

---
+++ providers/cy/ac/cut/apps.py
@@ -0,0 +1,10 @@
+from share.provider import OAIProviderAppConfig
+
+
+class AppConfig(OAIProviderAppConfig):
+    name = 'providers.cy.ac.cut'
+    version = '0.0.1'
+    title = 'cut'
+    long_title = 'Cyprus University of Technology'
+    home_page = 'http://ktisis.cut.ac.cy/'
+    url = 'http://ktisis.cut.ac.cy/oai/request'
