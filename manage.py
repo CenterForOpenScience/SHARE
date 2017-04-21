e6a3c358fe81fb77539d7b39dd0c8a15713c47d3
Lauren Barker <lauren@cos.io>
2017-04-21 13:32:56 -0400

    Subclass runserver command as runlocalserver

--- manage.py
+++ manage.py
@@ -2,8 +2,6 @@
 import os
 import sys
 
-import django
-
 if os.environ.get('GEVENT') == '1':
     from gevent import monkey
     monkey.patch_all()
@@ -13,11 +11,6 @@ if os.environ.get('GEVENT') == '1':
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
-    # Override default port for `runserver` command
-    django.setup()
-    from django.core.management.commands.runserver import Command as runserver
-    runserver.default_port = "38000"
 
     from django.core.management import execute_from_command_line
 
