55cb25eaffdc3a7ab9d889c8a9478bdcd5c6d44c
Lauren Barker <lauren@cos.io>
2017-04-25 10:22:40 -0400

    Separate out post only and get only url patterns
    Improve regex

--- api/urls.py
+++ api/urls.py
@@ -99,15 +99,17 @@ urlpatterns = [
     url(r'graph/?', GraphQLView.as_view(graphiql=True)),
     url(r'userinfo/?', ensure_csrf_cookie(views.ShareUserView.as_view()), name='userinfo'),
 
-    # only match _count, _search, _suggest requests
+    # only match _count and _search requests
     url(
-        r'search/(?!.*_bulk\/?$)(?P<url_bits>(_|[a-zA-Z]+/_)((?=search)|(?=count)|(?=suggest))[a-zA-Z]{5,7}/{0,1}$)',
+        r'search/(?P<url_bits>(_|[a-zA-Z]+/_)(?:search|count)/{0,1}$)',
         csrf_exempt(views.ElasticSearchView.as_view()),
         name='search'
     ),
+    # match _suggest requests
+    url(r'search/(?P<url_bits>(_|[a-zA-Z]+/_)(?:suggest)/{0,1}$)', csrf_exempt(views.ElasticSearchPostOnlyView.as_view()), name='search_post'),
     # match _mappings requests
-    url(r'search/(?P<url_bits>_mappings(/.+|$|/))', csrf_exempt(views.ElasticSearchView.as_view()), name='search'),
-    url(r'search/(?!.*_bulk\/?$)(?P<url_bits>.*)', csrf_exempt(views.ElasticSearch403View.as_view()), name='search_403'),
+    url(r'search/(?P<url_bits>_mappings(/.+|$|/))', csrf_exempt(views.ElasticSearchGetOnlyView.as_view()), name='search_get'),
+    url(r'search/(?P<url_bits>.*)', csrf_exempt(views.ElasticSearch403View.as_view()), name='search_403'),
 
     url(r'schema/?$', views.SchemaView.as_view(), name='schema'),
     url(r'schema/creativework/hierarchy/?$', views.ModelTypesView.as_view(), name='modeltypes'),
--- tests/api/test_elasticsearch.py
+++ tests/api/test_elasticsearch.py
@@ -20,10 +20,10 @@ class TestElasticSearchProxy:
         assert client.delete('/api/v2/search/type/index/_thing').status_code == 405
 
     def test_cannot_access_bulk(self, client):
-        assert client.delete('/api/v2/search/_bulk').status_code == 404
-        assert client.delete('/api/v2/search/_bulk?test').status_code == 404
-        assert client.delete('/api/v2/search/type/_bulk?foo').status_code == 404
-        assert client.delete('/api/v2/search/type/index/_bulk').status_code == 404
+        assert client.delete('/api/v2/search/_bulk').status_code == 405
+        assert client.delete('/api/v2/search/_bulk?test').status_code == 405
+        assert client.delete('/api/v2/search/type/_bulk?foo').status_code == 405
+        assert client.delete('/api/v2/search/type/index/_bulk').status_code == 405
 
     def test_limitted_post(self, client):
         assert client.post('/api/v2/search/type').status_code == 403
@@ -56,11 +56,24 @@ class TestElasticSearchProxy:
             '/api/v2/search/type/_search',
             '/api/v2/search/type/_search/',
             '/api/v2/search/type/_suggest',
+            '/api/v2/search/type/_suggest/',
         )
         with mock.patch('api.views.elasticsearch.requests.post') as post:
             post.return_value = mock.Mock(status_code=200, json=lambda: {})
             for url in urls:
                 assert client.post(url, '{}', content_type='application/json').status_code == 200
+
+    def test_cannot_post(self, client):
+        urls = (
+            '/api/v2/search/_mappings/',
+            '/api/v2/search/_mappings',
+            '/api/v2/search/_mappings/creativeworks',
+            '/api/v2/search/_mappings/creativeworks/',
+        )
+        with mock.patch('api.views.elasticsearch.requests.post') as post:
+            post.return_value = mock.Mock(status_code=405, json=lambda: {})
+            for url in urls:
+                assert client.post(url, '{}', content_type='application/json').status_code == 405
 
     def test_get_search(self, client):
         urls = (
@@ -79,3 +92,15 @@ class TestElasticSearchProxy:
             get.return_value = mock.Mock(status_code=200, json=lambda: {})
             for url in urls:
                 assert client.get(url).status_code == 200
+
+    def test_cannot_get(self, client):
+        urls = (
+            '/api/v2/search/_suggest',
+            '/api/v2/search/_suggest/',
+            '/api/v2/search/type/_suggest',
+            '/api/v2/search/type/_suggest/',
+        )
+        with mock.patch('api.views.elasticsearch.requests.get') as get:
+            get.return_value = mock.Mock(status_code=405, json=lambda: {})
+            for url in urls:
+                assert client.get(url).status_code == 405
