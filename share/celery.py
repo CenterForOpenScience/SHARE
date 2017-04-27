f01f11957df7af605b165c6d22f9510c1d00c9e6
Chris Seto <chriskseto@gmail.com>
2017-04-27 11:08:17 -0400

    [SHARE-815][Fix] Temporary fix for zombie celery workers
      Handle disconnects from Postgres in the result backend layer by
      crashing the entire worker.

--- share/celery.py
+++ share/celery.py
@@ -1,4 +1,18 @@
+import logging
+
+import raven
+
 from django.conf import settings
+
+from djcelery.backends.database import DatabaseBackend as DjCeleryDatabaseBackend
+
+
+logger = logging.getLogger(__name__)
+
+if hasattr(settings, 'RAVEN_CONFIG') and settings.RAVEN_CONFIG['dsn']:
+    client = raven.Client(settings.RAVEN_CONFIG['dsn'])
+else:
+    client = None
 
 
 def match_by_module(task_path):
@@ -21,3 +35,62 @@ class CeleryRouter(object):
         return {
             'queue': match_by_module(task)
         }
+
+
+class DatabaseBackend(DjCeleryDatabaseBackend):
+
+    def _handle_unhandled_exception(self, exc):
+        try:
+            logger.exception(exc)
+            logger.critical('Caught an unhandled exception in the result backend, killing process')
+            if client:
+                client.capture_exceptions(sample_rate=0.5)
+        except Exception as e:
+            logger.exception()
+            logger.critical('Failed to log a previous failure')
+            if client:
+                client.capture_exceptions(sample_rate=0.5)
+        finally:
+            raise SystemExit(57)  # Something a bit less generic than 1 or -1
+
+    def _store_result(self, task_id, result, status, traceback=None, request=None):
+        try:
+            return super()._store_result(task_id, result, status, traceback=traceback, request=result)
+        except Exception as e:
+            self._handle_unhandled_exception(e)
+
+    def _save_group(self, group_id, result):
+        try:
+            return super()._save_group(group_id, result)
+        except Exception as e:
+            self._handle_unhandled_exception(e)
+
+    def _get_task_meta_for(self, task_id):
+        try:
+            return super()._get_task_meta_for(task_id)
+        except Exception as e:
+            self._handle_unhandled_exception(e)
+
+    def _restore_group(self, group_id):
+        try:
+            return super()._restore_group(group_id)
+        except Exception as e:
+            self._handle_unhandled_exception(e)
+
+    def _delete_group(self, group_id):
+        try:
+            return super()._delete_group(group_id)
+        except Exception as e:
+            self._handle_unhandled_exception(e)
+
+    def _forget(self, task_id):
+        try:
+            return super()._forget(task_id)
+        except Exception as e:
+            self._handle_unhandled_exception(e)
+
+    def cleanup(self):
+        try:
+            return super().cleanup()
+        except Exception as e:
+            self._handle_unhandled_exception(e)
