4edb803926bd131edcc954d6d9adb61143e7cdf2
Emily Carden <cardene19@mail.wlu.edu>
2017-06-19 14:43:37 -0400

    Cherry pick

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,7 @@ from rest_framework_json_api import serializers
 
 from share import models
 
-from share.models import ProviderRegistration, SiteBanner, CeleryTaskResult, HarvestLog
+from share.models import ProviderRegistration, SiteBanner, CeleryTaskResult, HarvestLog, SourceConfig
 
 
 from share.models import ProviderRegistration, SiteBanner, CeleryTaskResult, logs
