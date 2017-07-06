5d5f9041b759cec5aeb89a0c47932afe70c1493f
Sherif Abdelhamid <sherif@cos.io>
2017-07-06 10:52:19 -0400

    [SHARE-832][feature]Add Symbiota harvester (#671)
    * [SHARE-832][Feature]Add Symbiota harvester
    * Update setup
    * Add harvester
    * Add transformer
    * Add source
    * Add tests for harvester/transformer
    
    * Update code
    
    * Update code
    
    * Add requests-mock to requirements
    
    * Rename test files
    
    * Apply code review
    
    * Use name parser
    
    * * Use httpretty and apply other code review requests
    
    * Add httpretty to dev-requirements

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
             'org.plos = share.transformers.org_plos:PLoSTransformer',
             'org.psyarxiv = share.transformers.org_psyarxiv:PsyarxivTransformer',
             'org.socarxiv = share.transformers.org_socarxiv:SocarxivTransformer',
+            'org.swbiodiversity = share.transformers.org_swbiodiversity:SWTransformer',
             'v1_push = share.transformers.v1_push:V1Transformer',
         ],
         'share.harvesters': [
@@ -91,6 +92,7 @@ setup(
             'org.ncar = share.harvesters.org_ncar:NCARHarvester',
             'org.neurovault = share.harvesters.org_neurovault:NeuroVaultHarvester',
             'org.plos = share.harvesters.org_plos:PLOSHarvester',
+            'org.swbiodiversity = share.harvesters.org_swbiodiversity:SWHarvester',
         ]
     }
 )
