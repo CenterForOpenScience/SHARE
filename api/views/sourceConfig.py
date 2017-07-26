b30f9aa1c5f19b8dad261e646875be88edd881a5
Emily Carden <cardene19@mail.wlu.edu>
2017-07-26 15:39:32 -0400

    working on sorting by health

--- api/views/sourceConfig.py
+++ api/views/sourceConfig.py
@@ -4,11 +4,26 @@ from rest_framework import filters
 from api.serializers import SourceConfigSerializer
 from share.models import SourceConfig
 from api.pagination import FuzzyPageNumberPagination
+from django.db.models import Count
+from django.db.models import Aggregate
+from share.models import HarvestLog
+from django.db.models import OuterRef, Subquery
 
 class SourceConfigViewSet(ShareObjectViewSet):
     serializer_class = SourceConfigSerializer
-    queryset= SourceConfig.objects.all().order_by('harvest_logs__status')
     pagination_class = FuzzyPageNumberPagination
-    for i in queryset:
-        print(i)
-        print("harvest logs:", i.harvest_logs)
+
+    def get_queryset(self):
+        recent_harvests = HarvestLog.objects.filter(
+            source_config_id = OuterRef(OuterRef('id')),
+            status__in = [HarvestLog.STATUS.failed, HarvestLog.STATUS.succeeded]
+            ).order_by('-date_started')
+        recent_fails = HarvestLog.objects.filter(
+            status = HarvestLog.STATUS.failed,
+            id__in = Subquery(recent_harvests.values('id'))
+        )
+        queryset = SourceConfig.objects.all().annotate(
+            fails=Count(Subquery(recent_fails.values('id')))
+            )
+        queryset = queryset.order_by('fails')
+        return queryset
