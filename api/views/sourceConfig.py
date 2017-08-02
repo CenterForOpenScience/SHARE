41e5c29f89b87a0526f5045679d8b24efe3e323e
Emily Carden <cardene19@mail.wlu.edu>
2017-08-02 14:27:48 -0400

    commented out sort by health

--- api/views/sourceConfig.py
+++ api/views/sourceConfig.py
@@ -13,20 +13,21 @@ from django.db.models import IntegerField
 class SourceConfigViewSet(ShareObjectViewSet):
     serializer_class = SourceConfigSerializer
     pagination_class = FuzzyPageNumberPagination
+    queryset = SourceConfig.objects.all()
 
-    def get_queryset(self):
-        recent_harvests = HarvestLog.objects.filter(
-            source_config_id = OuterRef(OuterRef('id')),
-            status__in = [HarvestLog.STATUS.failed, HarvestLog.STATUS.succeeded]
-            ).order_by('-date_started')[:10]
-        recent_fails = HarvestLog.objects.filter(
-            status = HarvestLog.STATUS.failed,
-            id__in = Subquery(recent_harvests.values('id')),
-            # output_field = models.IntegerField())
-            )
-        queryset = SourceConfig.objects.all().annotate(
-            fails=Count(Subquery(recent_fails.values('id')),
-            # output_field = models.IntegerField()))
-            )
-        queryset = queryset.order_by('fails')
-        return queryset
+    # def get_queryset(self):
+    #     recent_harvests = HarvestLog.objects.filter(
+    #         source_config_id = OuterRef(OuterRef('id')),
+    #         status__in = [HarvestLog.STATUS.failed, HarvestLog.STATUS.succeeded]
+    #         ).order_by('-date_started')[:10]
+    #     recent_fails = HarvestLog.objects.filter(
+    #         status = HarvestLog.STATUS.failed,
+    #         id__in = Subquery(recent_harvests.values('id')),
+    #         # output_field = models.IntegerField())
+    #         )
+    #     queryset = SourceConfig.objects.all().annotate(
+    #         fails=Count(Subquery(recent_fails.values('id')),
+    #         # output_field = models.IntegerField()))
+    #         )
+    #     queryset = queryset.order_by('fails')
+    #     return queryset
