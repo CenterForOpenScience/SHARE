b1200f16a99fe315225d911551b46460059d62ba
Emily Carden <cardene19@mail.wlu.edu>
2017-08-08 14:56:24 -0400

    Cherry pick

--- api/views/harvestlogs.py
+++ api/views/harvestlogs.py
@@ -1,32 +1,23 @@
 from rest_framework import viewsets
 from rest_framework import filters
+from django_filters.filters import MultipleChoiceFilter
 from api.views import ShareObjectViewSet
 from share.util import IDObfuscator
 
-from rest_framework import viewsets, views
-# from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
 from api.serializers import HarvestLogSerializer
 from share.models import HarvestLog
-from api.views import ShareObjectViewSet
 
-
-class SourceConfigFilterBackend(filters.BaseFilterBackend):
-    """
-    Filter that only allows users to see their own objects.
-    """
-    def filter_queryset(self, request, queryset, view):
+class SourceConfigFilterBackend(MultipleChoiceFilter):
+    def filter_queryset(self, request, queryset, view, conjoined=True):
         if 'source_config_id' in request.GET:
             decoded = IDObfuscator.decode_id(request.GET['source_config_id'])
-            return queryset.filter(source_config_id=decoded)
+            queryset = queryset.filter(source_config_id=decoded)
         if 'status' in request.GET:
-            return queryset.filter(status=request.GET['status'])
-
-        else:
-            return queryset
+            queryset = queryset.filter(status__in=request.GET.getlist('status'))
+        return queryset
 
 class HarvestLogViewSet(ShareObjectViewSet):
-    queryset= HarvestLog.objects.all()
     serializer_class = HarvestLogSerializer
+    queryset = HarvestLog.objects.all()
     filter_backends = (SourceConfigFilterBackend, )
-    filter_fields = ['source_config__id', 'status',]
+    filter_fields = ('source_config_id', 'status',)
