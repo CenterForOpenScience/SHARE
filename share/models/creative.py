0fbf4fbac348d746fc236de44a0b0fccf9f45c3b
Chris Seto <chriskseto@gmail.com>
2017-08-10 13:02:11 -0400

    [Feature] Defrankenize (#696)
    * [Feature] Defrankenize
    
    * Better testing
    
    * Clean up
    
    * Fix merge conflicts

--- share/models/creative.py
+++ share/models/creative.py
@@ -1,16 +1,22 @@
+import logging
+
 from django.db import models
+from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
 
 from share.models.base import ShareObject
 from share.models.base import TypedShareObjectMeta
 from share.models.meta import Tag, Subject
+from share.models.change import Change
 from share.models.fields import ShareManyToManyField, ShareURLField
 
 from share.util import strip_whitespace, ModelGenerator
 
 
-# Base Creative Work class
+logger = logging.getLogger(__name__)
 
+
+# Base Creative Work class
 class AbstractCreativeWork(ShareObject, metaclass=TypedShareObjectMeta):
     title = models.TextField(blank=True, help_text='')
     description = models.TextField(blank=True, help_text='')
@@ -42,6 +48,32 @@ class AbstractCreativeWork(ShareObject, metaclass=TypedShareObjectMeta):
     class Meta(ShareObject.Meta):
         db_table = 'share_creativework'
         verbose_name_plural = 'Creative Works'
+
+    def defrankenize(self, *_, im_really_sure_about_this=False):
+        if not im_really_sure_about_this:
+            raise ValueError('You have to be really sure about this')
+
+        logger.info('Defrankenizing %r', self)
+
+        with transaction.atomic():
+            logger.info('Removing relations')
+            for field in AbstractCreativeWork._meta.get_fields():
+                if not field.one_to_many or field.name in ('changes', 'versions'):
+                    continue
+
+                logger.warning('Removing all %s', field.related_name)
+                relation = getattr(self, field.get_accessor_name())
+                num_deleted, stats = Change.objects.filter(id__in=relation.values_list('change_id', flat=True)).delete()
+                logger.warning('Deleted %d changes to remove %s', num_deleted, field.related_name)
+
+                assert num_deleted == stats.pop('share.Change', 0)
+
+                if stats:
+                    logger.error('Unexpectedly removed other rows, %r', stats)
+                    raise ValueError('Unexpectedly removed other rows, {!r}'.format(stats))
+
+            logger.info('Relations removed')
+            self.administrative_change(is_deleted=True, title='Defrankenized work')
 
     def __str__(self):
         return self.title
---
+++ tests/factories/core.py
@@ -0,0 +1,40 @@
+import factory
+import faker
+from factory.django import DjangoModelFactory
+
+from share import models
+
+__all__ = (
+    'SourceFactory',
+    'ShareUserFactory',
+    'NormalizedDataFactory',
+)
+
+faker = faker.Faker()
+
+
+class ShareUserFactory(DjangoModelFactory):
+    username = factory.Sequence(lambda x: '{}{}'.format(faker.name(), x))
+    source = factory.RelatedFactory('tests.factories.core.SourceFactory', 'user')
+
+    class Meta:
+        model = models.ShareUser
+
+
+class NormalizedDataFactory(DjangoModelFactory):
+    data = {}
+    source = factory.SubFactory(ShareUserFactory)
+
+    class Meta:
+        model = models.NormalizedData
+
+
+class SourceFactory(DjangoModelFactory):
+    name = factory.Faker('sentence')
+    long_title = factory.Faker('sentence')
+    icon = factory.SelfAttribute('name')
+
+    user = factory.SubFactory(ShareUserFactory, source=None)
+
+    class Meta:
+        model = models.Source
