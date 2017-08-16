36134001c31243915983a195780af147667254e6
Chris Seto <chriskseto@gmail.com>
2017-08-16 17:36:31 -0400

    [Fix] Make more generic

--- share/management/commands/fix_datacite.py
+++ share/management/commands/fix_datacite.py
@@ -11,6 +11,7 @@ from share.models import WorkIdentifier, Preprint, ShareUser
 class Command(BaseCommand):
 
     OSF_PP = ['OSF', 'engrXiv', 'PsyArXiv', 'BITSS', 'LIS Scholarship Archive', 'SocArXiv', 'LawArXiv', 'AgriXiv', 'MindRxiv', 'Open Science Framework']
+    AGGREGATORS = ['DataCite MDS', 'CrossRef']
 
     def add_arguments(self, parser):
         parser.add_argument('--commit', action='store_true', help='Should the script actually commit?')
@@ -23,11 +24,11 @@ class Command(BaseCommand):
             graveyard = WorkIdentifier.objects.get(uri='http://osf.io/8bg7d/').creative_work
             self.stdout.write(self.style.SUCCESS('Found the Graveyard @ "{}"'.format(graveyard.id)))
 
-            DATACITE_USER = ShareUser.objects.get(source__long_title='DataCite MDS')
             OSF_PP_USER_IDS = list(ShareUser.objects.filter(source__long_title__in=self.OSF_PP).values_list('id', flat=True))
+            AGGREGATOR_USER_IDS = ShareUser.objects.filter(source__long_title__in=self.AGGREGATORS).values_list('id', flat=True)
 
             pps = Preprint.objects.filter(
-                sources=DATACITE_USER,
+                sources__in=AGGREGATOR_USER_IDS,
                 date_modified__gte='2017-07-30',
                 id__in=Preprint.objects.filter(sources__in=OSF_PP_USER_IDS)
             )
@@ -47,35 +48,26 @@ class Command(BaseCommand):
                 if not dupes:
                     continue
 
-                self.stdout.write('=== Processing Work "{}" ==='.format(work.id))
-
-                # Some cases will have to be manually inspected
-                if work.id in (26335370, 16423465, 28708935):
-                    self.stdout.write(self.style.NOTICE('Lost cause: "{}"'.format(work.id)))
-                    continue
+                self.stdout.write('=== Processing Work "{}" Modified On {} ==='.format(work.id, work.date_modified))
 
                 for agents in dupes.values():
-                    # Find the core OSF agent
-                    osf = [agent for agent in agents if agent.sources.filter(source__long_title__in=self.OSF_PP)]
-                    if not osf:
-                        self.stdout.write(self.style.NOTICE('No agents from an OSF source. {}, {}'.format(agents, work.id)))
-                        continue
-                    if len(osf) > 1:
-                        self.stdout.write('Found duplicates from OSF sources. Picking the one with the most identifiers. {}'.format(osf))
-                        osf = list(sorted(osf, key=lambda x: x.identifiers.count(), reverse=True))
+                    # Order by # of identifiers and a preference towards OSF sources
+                    core_agent = list(sorted(agents, key=lambda x: (agent.sources.filter(source__long_title__in=self.OSF_PP).exists(), x.identifiers.count()), reverse=True))[0]
 
-                    osf = osf[0]
-
-                    self.stdout.write('\t=== Processing Agent "{}" ==='.format(agent.id))
-                    self.stdout.write('\tSmashing {} into {}'.format([a for a in agents if a != osf], osf))
+                    self.stdout.write('\tSmashing {} into {} from {} identified by {}'.format(
+                        [a for a in agents if a != core_agent],
+                        core_agent,
+                        core_agent.sources.values_list('source__long_title', flat=True),
+                        core_agent.identifiers.values_list('uri', flat=True),
+                    ))
 
                     for agent in agents:
-                        if agent == osf:
+                        if agent == core_agent:
                             continue
                         for identifier in agent.identifiers.all():
-                            self.stdout.write('\t\tRepointing {}: {} -> {}'.format(identifier.uri, agent, osf))
+                            self.stdout.write('\t\tRepointing {}: {} -> {}'.format(identifier.uri, agent, core_agent))
                             if not options.get('dry'):
-                                identifier.administrative_change(agent=osf)
+                                identifier.administrative_change(agent=core_agent)
 
                         for rel in agent.work_relations.all():
                             self.stdout.write('\t\tReassigning {}: {} -> {}'.format(rel, rel.creative_work.id, graveyard.id))
@@ -86,7 +78,7 @@ class Command(BaseCommand):
                 self.stdout.write('Bumping last_modified on work')
                 if not options.get('dry'):
                     work.administrative_change(allow_empty=True)
-                self.stdout.write(self.style.SUCCESS('Successfully Processed Work "{}\n"'.format(work.id)))
+                self.stdout.write(self.style.SUCCESS('Successfully Processed Work "{}"\n'.format(work.id)))
 
             if not options.get('commit'):
                 raise ValueError('not_dry not set, rolling backing')
