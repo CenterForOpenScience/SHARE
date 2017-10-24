af3c3e51c2e1380692b3955a043c37749372f270
Abram Booth <abram@cos.io>
2017-10-24 17:18:32 -0400

    Cleaner rollback logic

--- share/management/commands/__init__.py
+++ share/management/commands/__init__.py
@@ -4,6 +4,10 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 
 
+class Rollback(Exception):
+    pass
+
+
 class BaseShareCommand(BaseCommand):
 
     @contextmanager
@@ -15,7 +19,6 @@ class BaseShareCommand(BaseCommand):
 
                 if not commit:
                     self.stdout.write('\nRolling back changes...', style_func=self.style.NOTICE)
-                    raise AssertionError('ROLLBACK')
-        except AssertionError as e:
-            if e.args != ('ROLLBACK', ):
-                raise
+                    raise Rollback
+        except Rollback:
+            pass
