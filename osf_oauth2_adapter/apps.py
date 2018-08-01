c6e1f22c44aef15f11b71a97a68864146e4078e6
Abram Booth <abram@cos.io>
2018-08-01 14:47:11 -0400

    Update default CAS URL

--- osf_oauth2_adapter/apps.py
+++ osf_oauth2_adapter/apps.py
@@ -7,6 +7,6 @@ class OsfOauth2AdapterConfig(AppConfig):
     name = 'osf_oauth2_adapter'
     # staging by default so people don't have to run OSF to use this.
     osf_api_url = os.environ.get('OSF_API_URL', 'https://staging-api.osf.io').rstrip('/') + '/'
-    osf_accounts_url = os.environ.get('OSF_ACCOUNTS_URL', 'https://staging-accounts.osf.io').rstrip('/') + '/'
+    osf_accounts_url = os.environ.get('OSF_ACCOUNTS_URL', 'https://accounts.staging.osf.io').rstrip('/') + '/'
     default_scopes = ['osf.users.profile_read', ]
     humans_group_name = 'OSF_USERS'
