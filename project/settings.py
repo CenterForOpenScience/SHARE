754b0ae0b150e972a9278e64894d2036de1f375d
Abram Booth <abram@cos.io>
2018-08-16 12:18:26 -0400

    Update default OSF API URLs

--- project/settings.py
+++ project/settings.py
@@ -530,7 +530,7 @@ SHARE_API_URL = os.environ.get('SHARE_API_URL', 'http://localhost:8000').rstrip(
 SHARE_WEB_URL = os.environ.get('SHARE_WEB_URL', SHARE_API_URL + EMBER_SHARE_PREFIX).rstrip('/') + '/'
 SHARE_USER_AGENT = os.environ.get('SHARE_USER_AGENT', 'SHAREbot/{} (+{})'.format(VERSION, SHARE_WEB_URL))
 
-OSF_API_URL = os.environ.get('OSF_API_URL', 'https://staging-api.osf.io').rstrip('/') + '/'
+OSF_API_URL = os.environ.get('OSF_API_URL', 'https://api.osf.io').rstrip('/') + '/'
 OSF_BYPASS_THROTTLE_TOKEN = os.environ.get('BYPASS_THROTTLE_TOKEN', None)
 
 DOI_BASE_URL = os.environ.get('DOI_BASE_URL', 'http://dx.doi.org/')
