08fe250cce0e106183732db9f9b285b51a14a171
Abram Booth <abram@cos.io>
2020-09-14 11:42:11 -0400

    feat: static schema definition
    instead of inspecting ShareObject and its subclasses,
    use share.schema.ShareV2Schema for all your SHARE schema needs

---
+++ share/schema/exceptions.py
@@ -0,0 +1,5 @@
+from share.exceptions import ShareException
+
+
+class SchemaLoadError(ShareException):
+    pass
---
+++ share/schema/shapes.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from typing import Set, NamedTuple, Optional
+
+
+RelationShape = Enum('RelationShape', ['MANY_TO_MANY', 'MANY_TO_ONE', 'ONE_TO_MANY'])
+AttributeDataType = Enum('AttributeDataType', ['BOOLEAN', 'STRING', 'INTEGER', 'DATETIME', 'OBJECT'])
+AttributeDataFormat = Enum('AttributeDataFormat', ['URI'])
+
+
+class ShareV2SchemaType(NamedTuple):
+    name: str
+    concrete_type: str
+    explicit_fields: Set[str]
+    distance_from_concrete_type: int = 0
+
+
+class ShareV2SchemaAttribute(NamedTuple):
+    name: str
+    data_type: AttributeDataType
+    data_format: Optional[AttributeDataFormat]
+    is_required: bool = False
+    is_relation: bool = False
+
+
+class ShareV2SchemaRelation(NamedTuple):
+    name: str
+    relation_shape: RelationShape
+    related_concrete_type: str
+    inverse_relation: str
+    is_required: bool = False
+    is_implicit: bool = False
+    is_relation: bool = True
