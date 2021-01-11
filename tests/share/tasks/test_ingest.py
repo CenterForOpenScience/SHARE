86a3c5c38b769956f8712de24237a5ed1e88647d
Abram Booth <abram@cos.io>
2021-01-11 09:07:02 -0500

    update how IngestJobs are consumed
    - ignore IngestJob.raw -- instead, always consume the most recent RawDatum
      for the given IngestJob.suid (anticipating a future in which
      IngestJob.suid is one-to-one and IngestJob.raw is removed)
    - add a `pls_format_metadata` parameter (default True) -- separate the
      legacy/ShareObject ingestion path (controlled by the `apply_changes`
      param) from the new FormattedMetadataRecord path
    - add optional `metadata_formats` param -- if given, limits the formats
      of FMRs that will be created during ingestion (default to all formats)

--- tests/share/tasks/test_ingest.py
+++ tests/share/tasks/test_ingest.py
@@ -1,6 +1,5 @@
 import json
 
-import pendulum
 import pytest
 
 from share.tasks import ingest
@@ -9,16 +8,6 @@ from tests import factories
 
 @pytest.mark.django_db
 class TestIngestJobConsumer:
-    def test_pointless(self):
-        job = factories.IngestJobFactory(raw__datestamp=pendulum.now().subtract(hours=2))
-        factories.IngestJobFactory(suid=job.suid, raw__datestamp=pendulum.now().subtract(hours=1))
-
-        ingest(job_id=job.id)
-
-        job.refresh_from_db()
-        assert job.status == job.STATUS.skipped
-        assert job.error_context == job.SkipReasons.pointless.value
-
     def test_no_output(self):
         raw = factories.RawDatumFactory(datum=json.dumps({
             '@graph': []
