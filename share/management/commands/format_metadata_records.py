816b57307bc788d9a73a17ab7c2c712d60258256
Abram Booth <abram@cos.io>
2021-01-12 07:56:40 -0500

    improve format_metadata_records behavior
    be more careful of memory usage -- explicitly chunk into separate
    queries, rather than relying on server-side cursors via uncertain django
    api

--- share/management/commands/format_metadata_records.py
+++ share/management/commands/format_metadata_records.py
@@ -6,14 +6,25 @@ from share.tasks import ingest
 from share.util.extensions import Extensions
 
 
-# get most recent job.id for each suid
+# find most recent job for each suid, set its status back to `created`,
+# and return the job and suid ids
 # TODO clean up significantly once ingest jobs and suids are one-to-one
-ingest_job_sql = '''
-SELECT DISTINCT ON (job.suid_id) job.id, job.suid_id
-FROM share_ingestjob AS job
-WHERE job.suid_id >= %(suid_start_id)s
-ORDER BY job.suid_id ASC, job.date_started DESC NULLS LAST, job.date_created DESC
+update_ingest_job_sql = '''
+WITH latest_job_per_suid AS (
+    SELECT DISTINCT ON (latest_job.suid_id) latest_job.id
+    FROM share_ingestjob AS latest_job
+    WHERE latest_job.suid_id >= %(suid_start_id)s
+    ORDER BY latest_job.suid_id ASC, latest_job.date_started DESC NULLS LAST, latest_job.date_created DESC
+    LIMIT %(chunk_size)s
+)
+UPDATE share_ingestjob AS job
+SET status=%(set_job_status)s
+FROM latest_job_per_suid
+WHERE job.id = latest_job_per_suid.id AND job.status != %(in_progress_status)s
+RETURNING job.id, job.suid_id
 '''
+
+CHUNK_SIZE = 2000
 
 
 class Command(BaseShareCommand):
@@ -24,6 +35,7 @@ class Command(BaseShareCommand):
     def handle(self, *args, **options):
         metadata_formats = options['metadata_formats']
         suid_start_id = options['suid_start_id']
+
         valid_formats = Extensions.get_names('share.metadata_formats')
         if not metadata_formats:
             self.stdout.write(f'Valid metadata formats: {metadata_formats}')
@@ -33,22 +45,33 @@ class Command(BaseShareCommand):
             self.stderr.write(f'Invalid metadata format(s): {invalid_formats}. Valid formats: {valid_formats}')
             return
 
-        # giving the cursor a name makes it a server-side cursor
-        with connection._cursor(name='most_recent_job_per_suid') as cursor:
-            cursor.execute(ingest_job_sql, {'suid_start_id': suid_start_id})
+        with connection.cursor() as cursor:
             while True:
-                result_rows = cursor.fetchmany(size=2000)
-                if not result_rows:
-                    self.stdout.write('all done!')
+                last_successful_suid = self.enqueue_job_chunk(cursor, suid_start_id, metadata_formats)
+                if last_successful_suid is None:
                     break
-                last_suid_id = result_rows[-1][1]
-                job_ids = [result_row[0] for result_row in result_rows]
-                IngestJob.objects.filter(id__in=job_ids).update(status=IngestJob.STATUS.created)
-                for job_id in job_ids:
-                    ingest.delay(
-                        job_id=job_id,
-                        apply_changes=False,  # skip the whole ShareObject mess
-                        pls_format_metadata=True,
-                        metadata_formats=metadata_formats,
-                    )
-                self.stdout.write(f'queued tasks for {len(job_ids)} IngestJobs (last suid: {last_suid_id})...')
+                suid_start_id = int(last_successful_suid) + 1
+
+    def enqueue_job_chunk(self, cursor, suid_start_id, metadata_formats):
+        cursor.execute(update_ingest_job_sql, {
+            'suid_start_id': suid_start_id or 0,
+            'chunk_size': CHUNK_SIZE,
+            'set_job_status': IngestJob.STATUS.created,
+            'in_progress_status': IngestJob.STATUS.started,
+        })
+        result_rows = cursor.fetchall()
+        if not result_rows:
+            self.stdout.write('all done!')
+            return None
+
+        last_suid_id = result_rows[-1][1]
+        for result_row in result_rows:
+            job_id = result_row[0]
+            ingest.delay(
+                job_id=job_id,
+                apply_changes=False,  # skip the whole ShareObject mess
+                pls_format_metadata=True,  # definitely don't skip this command's namesake
+                metadata_formats=metadata_formats,
+            )
+        self.stdout.write(f'queued tasks for {len(result_rows)} IngestJobs (last suid: {last_suid_id})...')
+        return last_suid_id
