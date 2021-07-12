eb96f0fbea96ad30887161fcebb81f71f607ad90
Abram Booth <boothaa@gmail.com>
2021-07-12 17:25:50 -0400

    chore: remove unused custom db cruft

--- db/backends/postgresql/schema.py
+++
@@ -1,8 +0,0 @@
-from django.db.backends.postgresql.schema import DatabaseSchemaEditor as PostgresDatabaseSchemaEditor
-
-from db import deletion
-
-
-class DatabaseSchemaEditor(PostgresDatabaseSchemaEditor):
-
-    sql_create_index_concurrently = 'CREATE INDEX CONCURRENTLY %(name)s ON %(table)s%(using)s (%(columns)s)%(extra)s'
