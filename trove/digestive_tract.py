c6ac459aad6869f804f190488f2710353fb46ff9
Abram Booth <boothaa@gmail.com>
2023-11-09 10:59:14 -0500

    fix: vocab ingestion

--- trove/digestive_tract.py
+++ trove/digestive_tract.py
@@ -133,7 +133,7 @@ def extract(raw: share_db.RawDatum, *, undelete_indexcards=False) -> list[trove_
             if (_iri != _focus_iri) and _iri.startswith(_focus_iri):
                 _term_tripledict = {_iri: copy.deepcopy(_twopledict)}
                 # ensure a link to the ontology (in case there's not already)
-                primitive_rdf.RdfGraph(_term_tripledict).add_triple(
+                primitive_rdf.RdfGraph(_term_tripledict).add(
                     (_iri, RDFS.isDefinedBy, _focus_iri),
                 )
                 _tripledicts_by_focus_iri[_iri] = _term_tripledict
