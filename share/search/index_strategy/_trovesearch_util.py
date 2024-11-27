CenterForOpenScience/SHARE

commit 7b68b96529864e94003519841aa15faac385b2e0

agglomulated changes from quest/minimal-nest

share/search/index_strategy/_trovesearch_util.py
@@ -1,0 +1,231 @@
+from __future__ import annotations
+import base64
+from collections import defaultdict
+import contextlib
+import dataclasses
+import datetime
+import functools
+import json
+import logging
+import typing
+
+from django.db.models import Exists, OuterRef
+from primitive_metadata import primitive_rdf as rdf
+
+from trove import models as trove_db
+from trove.trovesearch.search_params import (
+    is_globpath,
+)
+from trove.util.iris import get_sufficiently_unique_iri, is_worthwhile_iri
+from trove.vocab.namespaces import (
+    DCTERMS,
+    FOAF,
+    OSFMAP,
+    OWL,
+    RDFS,
+    SKOS,
+    TROVE,
+)
+from trove.vocab.osfmap import is_date_property
+
+
+_logger = logging.getLogger(__name__)
+
+
+###
+# type aliases
+
+Propertypath = tuple[str, ...]
+
+
+###
+# constants
+
+SKIPPABLE_PROPERTIES = (
+    OSFMAP.contains,  # too much, not helpful
+    OWL.sameAs,  # handled special
+)
+
+TITLE_PROPERTIES = (DCTERMS.title,)
+NAME_PROPERTIES = (FOAF.name, OSFMAP.fileName)
+LABEL_PROPERTIES = (RDFS.label, SKOS.prefLabel, SKOS.altLabel)
+NAMELIKE_PROPERTIES = (*TITLE_PROPERTIES, *NAME_PROPERTIES, *LABEL_PROPERTIES)
+
+VALUESEARCH_MAX = 234
+CARDSEARCH_MAX = 9997
+
+KEYWORD_LENGTH_MAX = 8191  # skip keyword terms that might exceed lucene's internal limit
+# (see https://www.elastic.co/guide/en/elasticsearch/reference/current/ignore-above.html)
+KEYWORD_MAPPING = {'type': 'keyword', 'ignore_above': KEYWORD_LENGTH_MAX}
+FLATTENED_MAPPING = {'type': 'flattened', 'ignore_above': KEYWORD_LENGTH_MAX}
+TEXT_MAPPING = {
+    'type': 'text',
+    'index_options': 'offsets',  # for highlighting
+}
+IRI_KEYWORD_MAPPING = {
+    'type': 'object',
+    'properties': {  # for indexing iri values two ways:
+        'exact': KEYWORD_MAPPING,  # the exact iri value (e.g. "https://foo.example/bar/")
+        'suffuniq': KEYWORD_MAPPING,  # "sufficiently unique" (e.g. "://foo.example/bar")
+    },
+}
+
+
+###
+# utilities
+
+def latest_rdf_for_indexcard_pks(indexcard_pks):
+    return (
+        trove_db.LatestIndexcardRdf.objects
+        .filter(indexcard_id__in=indexcard_pks)
+        .filter(Exists(
+            trove_db.DerivedIndexcard.objects
+            .filter(upriver_indexcard_id=OuterRef('indexcard_id'))
+            .filter(deriver_identifier__in=(
+                trove_db.ResourceIdentifier.objects
+                .queryset_for_iri(TROVE['derive/osfmap_json'])
+            ))
+        ))
+        .exclude(indexcard__deleted__isnull=False)
+        .select_related('indexcard__source_record_suid__source_config')
+        .prefetch_related('indexcard__focus_identifier_set')
+    )
+
+
+def propertypath_as_keyword(path: Propertypath) -> str:
+    return json.dumps(path if is_globpath(path) else [
+        get_sufficiently_unique_iri(_iri)
+        for _iri in path
+    ])
+
+
+def propertypath_as_field_name(path: Propertypath) -> str:
+    _path_keyword = propertypath_as_keyword(path)
+    return base64.urlsafe_b64encode(_path_keyword.encode()).decode()
+
+
+def suffuniq_iris(iris: typing.Iterable[str]) -> list[str]:
+    # deduplicates, may reorder
+    return list({
+        get_sufficiently_unique_iri(_iri)
+        for _iri in iris
+    })
+
+
+@dataclasses.dataclass
+class GraphWalk:
+    rdfdoc: rdf.RdfGraph
+    focus_iri: str
+    recursive: bool = True
+    iri_values: dict[Propertypath, set[str]] = dataclasses.field(
+        default_factory=lambda: defaultdict(set),
+    )
+    text_values: dict[Propertypath, set[rdf.Literal]] = dataclasses.field(
+        default_factory=lambda: defaultdict(set),
+    )
+    date_values: dict[Propertypath, set[datetime.date]] = dataclasses.field(
+        default_factory=lambda: defaultdict(set),
+    )
+    paths_walked: set[Propertypath] = dataclasses.field(default_factory=set)
+    _visiting: set[str] = dataclasses.field(default_factory=set)
+
+    def __post_init__(self):
+        for _walk_path, _walk_obj in self._walk_from_subject(self.focus_iri):
+            self.paths_walked.add(_walk_path)
+            if isinstance(_walk_obj, str):
+                self.iri_values[_walk_path].add(_walk_obj)
+            elif isinstance(_walk_obj, datetime.date):
+                self.date_values[_walk_path].add(_walk_obj)
+            elif is_date_property(_walk_path[-1]):
+                try:
+                    _parsed_date = datetime.date.fromisoformat(_walk_obj.unicode_value)
+                except ValueError:
+                    _logger.debug('skipping malformatted date "%s"', _walk_obj.unicode_value)
+                else:
+                    self.date_values[_walk_path].add(_parsed_date)
+            elif isinstance(_walk_obj, rdf.Literal):
+                self.text_values[_walk_path].add(_walk_obj.unicode_value)
+
+    def shortwalk(self, from_iri: str) -> GraphWalk:
+        return GraphWalk(
+            self.rdfdoc,
+            self.focus_iri,
+            recursive=False,
+        )
+
+    def _walk_from_subject(
+        self,
+        iri_or_blanknode: str | rdf.Blanknode,
+        path_so_far: tuple[str, ...] = (),
+    ) -> typing.Iterator[tuple[Propertypath, rdf.RdfObject]]:
+        '''walk the graph from the given subject, yielding (pathkey, obj) for every reachable object
+        '''
+        with self._visit(iri_or_blanknode):
+            _twoples = (
+                iri_or_blanknode
+                if isinstance(iri_or_blanknode, frozenset)
+                else self.rdfdoc.tripledict.get(iri_or_blanknode, {})
+            )
+            for _next_steps, _obj in walk_twoples(_twoples):
+                _path = (*path_so_far, *_next_steps)
+                yield (_path, _obj)
+                if self.recursive and isinstance(_obj, str) and (_obj not in self._visiting):
+                    # step further for iri or blanknode
+                    yield from self._walk_from_subject(_obj, path_so_far=_path)
+
+    @functools.cached_property
+    def paths_by_iri(self) -> defaultdict[str, set[Propertypath]]:
+        _paths_by_iri: defaultdict[str, set[Propertypath]] = defaultdict(set)
+        for _path, _iris in self.iri_values.items():
+            for _iri in _iris:
+                _paths_by_iri[_iri].add(_path)
+        return _paths_by_iri
+
+    def iri_synonyms(self, iri: str) -> set[str]:
+        # note: extremely limited inference -- assumes objects of owl:sameAs are not used as subjects
+        _synonyms = (
+            _synonym
+            for _synonym in self.rdfdoc.q(iri, OWL.sameAs)
+            if is_worthwhile_iri(_synonym)
+        )
+        return {iri, *_synonyms}
+
+    def iris_synonyms(self, iris: typing.Iterable[str]) -> set[str]:
+        return {
+            _synonym
+            for _iri in iris
+            for _synonym in self.iri_synonyms(_iri)
+        }
+
+    @contextlib.contextmanager
+    def _visit(self, focus_obj):
+        assert focus_obj not in self._visiting
+        self._visiting.add(focus_obj)
+        yield
+        self._visiting.discard(focus_obj)
+
+
+def walk_twoples(
+    twoples: rdf.RdfTwopleDictionary | rdf.Blanknode,
+) -> typing.Iterator[tuple[Propertypath, rdf.RdfObject]]:
+    if isinstance(twoples, frozenset):
+        _iter_twoples = (
+            (_pred, _obj)
+            for _pred, _obj in twoples
+            if _pred not in SKIPPABLE_PROPERTIES
+        )
+    else:
+        _iter_twoples = (
+            (_pred, _obj)
+            for _pred, _obj_set in twoples.items()
+            if _pred not in SKIPPABLE_PROPERTIES
+            for _obj in _obj_set
+        )
+    for _pred, _obj in _iter_twoples:
+        _path = (_pred,)
+        if isinstance(_obj, frozenset):
+            for _innerpath, _innerobj in walk_twoples(_obj):
+                _fullpath = (*_path, *_innerpath)
+                yield (_fullpath, _innerobj)
+        else:
+            yield (_path, _obj)

api/search/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 
 from api import authentication
 from share.search import exceptions
-from share.search.index_strategy import IndexStrategy
+from share.search import index_strategy
 
 
 class Sharev2ElasticSearchView(views.APIView):
@@ -32,7 +32,7 @@ class Sharev2ElasticSearchView(views.APIView):
         if 'scroll' in queryparams:
             return http.HttpResponseForbidden(reason='Scroll is not supported.')
         try:
-            specific_index = IndexStrategy.get_for_sharev2_search(requested_index_strategy)
+            specific_index = index_strategy.get_index_for_sharev2_search(requested_index_strategy)
         except exceptions.IndexStrategyError as error:
             raise http.Http404(str(error))
         try:

api/views/feeds.py
@@ -10,7 +10,7 @@ from furl import furl
 import pendulum
 import sentry_sdk
 
-from share.search import IndexStrategy
+from share.search import index_strategy
 from share.search.exceptions import IndexStrategyError
 from share.util.xml import strip_illegal_xml_chars
 
@@ -34,6 +34,8 @@ class MetadataRecordsRSS(Feed):
     description = 'Updates to the SHARE open dataset'
     author_name = 'SHARE'
 
+    _search_index: index_strategy.IndexStrategy.SpecificIndex
+
     def title(self, obj):
         query = json.dumps(obj.get('query', 'All'))
         return prepare_string('SHARE: Atom feed for query: {}'.format(query))
@@ -41,7 +43,7 @@ class MetadataRecordsRSS(Feed):
     def get_object(self, request):
         self._order = request.GET.get('order')
         elastic_query = request.GET.get('elasticQuery')
-        self._index_strategy = IndexStrategy.get_for_sharev2_search(request.GET.get('indexStrategy'))
+        self._search_index = index_strategy.get_index_for_sharev2_search(request.GET.get('indexStrategy'))
 
         if self._order not in {'date_modified', 'date_updated', 'date_created', 'date_published'}:
             self._order = 'date_modified'
@@ -62,7 +64,7 @@ class MetadataRecordsRSS(Feed):
 
     def items(self, obj):
         try:
-            json_response = self._index_strategy.pls_handle_search__sharev2_backcompat(
+            json_response = self._search_index.pls_handle_search__sharev2_backcompat(
                 request_body=obj,
             )
         except IndexStrategyError:

project/settings.py
@@ -314,52 +314,15 @@ ELASTICSEARCH = {
     'TIMEOUT': int(os.environ.get('ELASTICSEARCH_TIMEOUT', '45')),
     'CHUNK_SIZE': int(os.environ.get('ELASTICSEARCH_CHUNK_SIZE', 2000)),
     'MAX_RETRIES': int(os.environ.get('ELASTICSEARCH_MAX_RETRIES', 7)),
-    'INDEX_STRATEGIES': {},  # populated below based on environment
 }
 ELASTICSEARCH5_URL = (
     os.environ.get('ELASTICSEARCH5_URL')
-    or os.environ.get('ELASTICSEARCH_URL')
+    or os.environ.get('ELASTICSEARCH_URL')  # backcompat
 )
-if ELASTICSEARCH5_URL:
-    ELASTICSEARCH['INDEX_STRATEGIES']['sharev2_elastic5'] = {
-        'INDEX_STRATEGY_CLASS': 'share.search.index_strategy.sharev2_elastic5.Sharev2Elastic5IndexStrategy',
-        'CLUSTER_SETTINGS': {
-            'URL': ELASTICSEARCH5_URL,
-        },
-    }
 ELASTICSEARCH8_URL = os.environ.get('ELASTICSEARCH8_URL')
-if ELASTICSEARCH8_URL:
-    ELASTICSEARCH8_CERT_PATH = os.environ.get('ELASTICSEARCH8_CERT_PATH')
-    ELASTICSEARCH8_USERNAME = os.environ.get('ELASTICSEARCH8_USERNAME', 'elastic')
-    ELASTICSEARCH8_SECRET = os.environ.get('ELASTICSEARCH8_SECRET')
-    ELASTICSEARCH8_CLUSTER_SETTINGS = {
-        'URL': ELASTICSEARCH8_URL,
-        'AUTH': (
-            (ELASTICSEARCH8_USERNAME, ELASTICSEARCH8_SECRET)
-            if ELASTICSEARCH8_SECRET is not None
-            else None
-        ),
-        'CERT_PATH': ELASTICSEARCH8_CERT_PATH,
-    }
-    ELASTICSEARCH['INDEX_STRATEGIES'].update({
-        'sharev2_elastic8': {
-            'INDEX_STRATEGY_CLASS': 'share.search.index_strategy.sharev2_elastic8.Sharev2Elastic8IndexStrategy',
-            'CLUSTER_SETTINGS': ELASTICSEARCH8_CLUSTER_SETTINGS,
-        },
-        'trove_indexcard_flats': {
-            'INDEX_STRATEGY_CLASS': 'share.search.index_strategy.trove_indexcard_flats.TroveIndexcardFlatsIndexStrategy',
-            'CLUSTER_SETTINGS': ELASTICSEARCH8_CLUSTER_SETTINGS,
-        },
-    })
-DEFAULT_INDEX_STRATEGY_FOR_LEGACY_SEARCH = (
-    'sharev2_elastic5'
-    if ELASTICSEARCH5_URL
-    else (
-        'sharev2_elastic8'
-        if ELASTICSEARCH8_URL
-        else None
-    )
-)
+ELASTICSEARCH8_CERT_PATH = os.environ.get('ELASTICSEARCH8_CERT_PATH')
+ELASTICSEARCH8_USERNAME = os.environ.get('ELASTICSEARCH8_USERNAME', 'elastic')
+ELASTICSEARCH8_SECRET = os.environ.get('ELASTICSEARCH8_SECRET')
 
 # Seconds, not an actual celery settings
 CELERY_RETRY_BACKOFF_BASE = int(os.environ.get('CELERY_RETRY_BACKOFF_BASE', 2 if DEBUG else 10))

share/admin/__init__.py
@@ -17,7 +17,7 @@ from share import tasks
 from share.admin.celery import CeleryTaskResultAdmin
 from share.admin.jobs import HarvestJobAdmin
 from share.admin.readonly import ReadOnlyAdmin
-from share.admin.search import search_indexes_view
+from share.admin.search import search_indexes_view, search_index_mappings_view
 from share.admin.util import TimeLimitedPaginator, linked_fk, linked_many, SourceConfigFilter
 from share.harvest.scheduler import HarvestScheduler
 from share.models import (
@@ -47,6 +47,11 @@ class ShareAdminSite(admin.AdminSite):
                 'search-indexes',
                 self.admin_view(search_indexes_view),
                 name='search-indexes',
+            ),
+            path(
+                'search-index-mappings/<index_name>',
+                self.admin_view(search_index_mappings_view),
+                name='search-index-mappings',
             ),
             *super().get_urls(),
         ]

share/admin/search.py
@@ -1,13 +1,13 @@
 import logging
 
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, JsonResponse
 from django.template.response import TemplateResponse
 from django.urls import reverse
 
 from share.admin.util import admin_url
 from share.models.index_backfill import IndexBackfill
 from share.search.index_messenger import IndexMessenger
-from share.search.index_strategy import IndexStrategy
+from share.search import index_strategy
 
 
 logger = logging.getLogger(__name__)
@@ -20,11 +20,12 @@ def search_indexes_view(request):
             'admin/search-indexes.html',
             context={
                 'search_url_prefix': _search_url_prefix(),
+                'mappings_url_prefix': _mappings_url_prefix(),
                 'index_status_by_strategy': _index_status_by_strategy(),
             },
         )
     if request.method == 'POST':
-        _specific_index = IndexStrategy.get_specific_index(request.POST['specific_indexname'])
+        _specific_index = index_strategy.get_specific_index(request.POST['specific_indexname'])
         _pls_doer = PLS_DOERS[request.POST['pls_do']]
         _pls_doer(_specific_index)
         _redirect_id = (
@@ -35,24 +36,35 @@ def search_indexes_view(request):
         return HttpResponseRedirect('#'.join((request.path, _redirect_id)))
 
 
+def search_index_mappings_view(request, index_name):
+    _specific_index = index_strategy.get_specific_index(index_name)
+    _mappings = _specific_index.pls_get_mappings()
+    return JsonResponse(_mappings)
+
+
 def _search_url_prefix():
     api_url = reverse('api:search')
     return f'{api_url}?indexStrategy='  # append strategyname or indexname
 
 
+def _mappings_url_prefix():
+    # return reverse('admin:search-index-mappings', kwargs={'index_name': ''})
+    return '/admin/search-index-mappings/'
+
+
 def _index_status_by_strategy():
-    backfill_by_indexname = {
+    backfill_by_indexname: dict[str, IndexBackfill] = {
         backfill.specific_indexname: backfill
         for backfill in (
             IndexBackfill.objects
-            .filter(index_strategy_name__in=IndexStrategy.all_strategies_by_name().keys())
+            .filter(index_strategy_name__in=index_strategy.all_index_strategies().keys())
         )
     }
     status_by_strategy = {}
     _messenger = IndexMessenger()
-    for index_strategy in IndexStrategy.all_strategies():
-        current_index = index_strategy.for_current_index()
-        status_by_strategy[index_strategy.name] = {
+    for _index_strategy in index_strategy.all_index_strategies().values():
+        current_index = _index_strategy.for_current_index()
+        status_by_strategy[_index_strategy.name] = {
             'current': {
                 'status': current_index.pls_get_status(),
                 'backfill': _serialize_backfill(
@@ -62,7 +74,7 @@ def _index_status_by_strategy():
             },
             'prior': sorted((
                 specific_index.pls_get_status()
-                for specific_index in index_strategy.each_specific_index()
+                for specific_index in _index_strategy.each_specific_index()
                 if not specific_index.is_current
             ), reverse=True),
             'queues': [
@@ -71,15 +83,18 @@ def _index_status_by_strategy():
                     **_messenger.get_queue_stats(_queue_name),
                 }
                 for _queue_name in (
-                    index_strategy.urgent_messagequeue_name,
-                    index_strategy.nonurgent_messagequeue_name,
+                    _index_strategy.urgent_messagequeue_name,
+                    _index_strategy.nonurgent_messagequeue_name,
                 )
             ],
         }
     return status_by_strategy
 
 
-def _serialize_backfill(specific_index: IndexStrategy.SpecificIndex, backfill: IndexBackfill):
+def _serialize_backfill(
+    specific_index: index_strategy.IndexStrategy.SpecificIndex,
+    backfill: IndexBackfill | None,
+):
     if not specific_index.is_current:
         return {}
     if not backfill:

share/bin/search.py
@@ -1,7 +1,7 @@
 from project.celery import app as celery_app
 
 from share.bin.util import command
-from share.search import IndexStrategy
+from share.search import index_strategy
 from share.search.exceptions import IndexStrategyError
 from share.search.daemon import IndexerDaemonControl
 
@@ -29,7 +29,7 @@ def purge(args, argv):
     Usage: {0} search purge <index_names>...
     """
     for index_name in args['<index_names>']:
-        specific_index = IndexStrategy.get_specific_index(index_name)
+        specific_index = index_strategy.get_specific_index(index_name)
         specific_index.pls_delete()
 
 
@@ -43,18 +43,16 @@ def setup(args, argv):
     if _is_initial:
         _specific_indexes = [
             _index_strategy.for_current_index()
-            for _index_strategy in IndexStrategy.all_strategies()
+            for _index_strategy in index_strategy.all_index_strategies().values()
         ]
     else:
         _index_or_strategy_name = args['<index_or_strategy_name>']
         try:
-            _specific_indexes = [
-                IndexStrategy.get_by_name(_index_or_strategy_name).for_current_index(),
-            ]
+            _specific_indexes = [index_strategy.get_specific_index(_index_or_strategy_name)]
         except IndexStrategyError:
             try:
                 _specific_indexes = [
-                    IndexStrategy.get_specific_index(_index_or_strategy_name),
+                    index_strategy.get_specific_index(_index_or_strategy_name),
                 ]
             except IndexStrategyError:
                 raise IndexStrategyError(f'unrecognized index or strategy name "{_index_or_strategy_name}"')

share/checks.py
@@ -2,18 +2,18 @@ from django.core import checks
 
 
 def check_all_index_strategies_current(app_configs, **kwargs):
-    from share.search import IndexStrategy
+    from share.search import index_strategy
     from share.search.exceptions import IndexStrategyError
     errors = []
-    for index_strategy in IndexStrategy.all_strategies():
+    for _index_strategy in index_strategy.all_index_strategies().values():
         try:
-            index_strategy.assert_strategy_is_current()
+            _index_strategy.assert_strategy_is_current()
         except IndexStrategyError as exception:
             errors.append(
                 checks.Error(
                     'IndexStrategy changed without checksum confirmation!',
                     hint=str(exception),
-                    obj=index_strategy,
+                    obj=_index_strategy,
                     id='share.search.E001',
                 )
             )

commit 9b23fad7f129ad756e9b28d5f8d1798ddfee6719

fix(trovesearch_denorm): whole-iris field names
retain the entire iri for path-based field names --
this makes them easily reversible for highlighting fields that match

(previously used "sufficiently unique" iris like "://foo.example/blarg",
which works well enough, except it'd take knowledge to get the original)

(bug introduced in 7b68b96529864e94003519841aa15faac385b2e0 )

share/search/index_strategy/_trovesearch_util.py
@@ -107,10 +107,7 @@ def iris_synonyms(iris: typing.Iterable[str], rdfdoc: rdf.RdfGraph) -> set[str]:
 
 def propertypath_as_keyword(path: Propertypath) -> str:
     assert not is_globpath(path)
-    return json.dumps([
-        get_sufficiently_unique_iri(_iri)
-        for _iri in path
-    ])
+    return json.dumps(path)
 
 
 def b64(value: str) -> str: