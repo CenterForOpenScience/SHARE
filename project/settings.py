947ba8df73aa0110fb995d9e24a5bbda6c95de3d
Bohdan Odintsov <bodintsov@exoft.net>
2025-04-07 17:50:42 +0300

    updated python version, updated django, updated containers, images, requirements

--- project/settings.py
+++ project/settings.py
@@ -276,9 +276,7 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
 ]
 
 
